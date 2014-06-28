c32acb80418f06ef783697b0cfe7e3b453921e32
rickfleehart <rickfleehart@yahoo.com>
2014-06-28 13:58:23 -0700

    Created BrushTeeth Console app from template.

---
+++ Rick.Fleehart/Homework1/BrushTeeth/BrushTeeth/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrushTeeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Teeth Brushing App 1.0");
+            Console.ReadKey();
+        }
+    }
+}
