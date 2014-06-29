7504cd0df82ea8190669c287548f99f4c51717b9
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-06-28 20:02:01 -0700

    create a folder

---
+++ Cathy.Blanscet/Homework1/BrushingTeeth/BrushingTeeth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrushingTeeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How to Brush your teeth");
+            Console.ReadKey();
+            /* insert code here to write out the instructions */
+            Console.WriteLine("instructions will go here...");
+            Console.ReadKey();
+            /* end of insert section */
+            Console.WriteLine("This should be done twice a day for proper health.");
+            Console.ReadKey();
+        }
+
+    }
+}
