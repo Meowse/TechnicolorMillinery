4a3625744585356bcac8971c4492b610b7a794d4
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-06-29 15:48:56 -0700

    Console App created.
    This is a named version, within my local repository, of the ConsoleApp
    solution/project that I made during Office Hours 1.5.

---
+++ Mickey.Phoenix/OfficeHours1.5/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.  If you can't find the 'Any' key, any other key will do.");
+            Console.ReadKey();
+        }
+    }
+}
