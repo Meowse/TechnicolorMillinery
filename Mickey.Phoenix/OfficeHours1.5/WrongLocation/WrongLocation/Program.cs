042490636f03330f87aaa642c2e1930bca6f9efd
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-06-29 15:49:16 -0700

    WrongLocation app
    This is the app that I made in the wrong location, and then committed
    here.

---
+++ Mickey.Phoenix/OfficeHours1.5/WrongLocation/WrongLocation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrongLocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ooops, I did it again, I misplaced my code, got lost on the disk");
+            Console.WriteLine("Oh baby, baby...");
+            Console.WriteLine("\n\n\n\nWait for user input...");
+            Console.ReadKey();
+        }
+    }
+}
