56534d84d4a9e44788468d63c90bcdef1c6d3aac
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-06-29 17:53:45 -0700

    Form Baseline
    this is a baseline for the form

---
+++ Cathy.Blanscet/Homework1/BrushTeethWFA/BrushTeethWFA/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrushTeethWFA
+{
+    public partial class Form1 : Form
+    {
+        private const string BrushingInstructions = "Acquire LiquidWasteReceptacle -- details TBD\n" +
+            "Acquire a toothbrush	-- details TBD\n" +
+            "CALL PROCEDURE \"Apply toothpaste to (the toothbrush we just acquired)\"\n" +
+            "Put the cap back on the toothpaste\n" +
+            "Put the toothpaste tube down\n" +
+            "Open mouth\n" +
+            "Place the toothpaste that is on the bristles against your teeth\n" +
+            "Move the toothbrush back and forth horizontally on the teeth\n" +
+            "Iterate motion over all the (exposed surfaces of the) teeth for two minutes\n" +
+            "Brush the tongue until you gag\n" +
+            "Remove toothbrush from your mouth\n" +
+            "Rinse the toothpaste out of your mouth and into the LiquidWasteReceptacle\n" +
+            "\n" +
+            "\n" +
+            "PROCEDURE \"Apply toothpaste to (the toothbrush we just acquired)\"\n" +
+            "	Acquire toothpaste tube -- details TBD\n" +
+            "	Pick up your toothbrush\n" +
+            "	Pick up your toothpaste\n" +
+            "	Unscrew the top\n" +
+            "	Align the bristles with the opening of the tube\n" +
+            "	Make sure the bristles are pointing up\n" +
+            "	Make sure that the toothbrush is below the toothpaste\n" +
+            "	Gently squeeze a small amount of the toothpaste onto the toothbrush\n" +
+            "		\"small\" -- the same size as the length and width of the bristles\"";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMeButtonClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show(BrushingInstructions, "Insructions on how to brush your teeth");
+        }
+
+        private void CloseMeButtonClicked(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
