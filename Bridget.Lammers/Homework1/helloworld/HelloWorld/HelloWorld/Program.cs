e66ba58fa876e0cee3ccceed90cfff7e6bb3cbf4
lammersb <bj_lammers@msn.com>
2014-06-29 18:52:18 -0700

    hello world more changes bridget

--- Bridget.Lammers/Homework1/helloworld/HelloWorld/HelloWorld/Program.cs
+++ Bridget.Lammers/Homework1/helloworld/HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+            Console.WriteLine("hello bridget");
             Console.ReadKey();
         }
     }
