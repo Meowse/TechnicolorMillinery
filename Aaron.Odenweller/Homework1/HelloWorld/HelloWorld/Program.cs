a82114d6e12f07479aa7605506b412ba5b7e2900
aodenwe <aaron.odenweller@gmail.com>
2014-06-30 09:43:58 -0700

    Homework 1 complete!

--- Aaron.Odenweller/Homework1/HelloWorld/HelloWorld/Program.cs
+++ Aaron.Odenweller/Homework1/HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
+        
     }
 }
