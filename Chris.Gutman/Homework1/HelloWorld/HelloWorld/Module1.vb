75ac0f75a9aabf3c1f0c77ddee98089e5342eb9e
dcgutman <chrisgutman@gmail.com>
2014-06-30 20:31:09 -0500

    Attempted conversion to C#

--- Chris.Gutman/Homework1/HelloWorld/HelloWorld/Module1.vb
+++ Chris.Gutman/Homework1/HelloWorld/HelloWorld/Module1.vb
@@ -1,7 +1,39 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hello World")
+
+        Console.WriteLine("How to brush your teeth:")
+        Console.WriteLine("Acquire LiquidWasteReceptacle -- details TBD")
+        Console.WriteLine("Acquire a toothbrush    -- details TBD")
+        Console.WriteLine("Apply toothpaste to (the toothbrush we just acquired)")
+        Console.WriteLine("Acquire toothpaste tube -- details TBD")
+        Console.WriteLine("Pick up your toothbrush")
+        Console.WriteLine("Pick up your toothpaste")
+        Console.WriteLine("Unscrew the top")
+        Console.WriteLine("Align the bristles with the opening of the tube")
+        Console.WriteLine("Make sure the bristles are pointing up")
+        Console.WriteLine("Make sure that the toothbrush is below the toothpaste")
+        Console.WriteLine("Gently squeeze a small amount of the toothpaste onto the toothbrush")
+        Console.WriteLine("small -- the same size as the length and width of the bristles")
+        Console.WriteLine("Put the cap back on the toothpaste")
+        Console.WriteLine("Put the toothpaste tube down")
+        Console.WriteLine("Open mouth")
+        Console.WriteLine("Place the toothpaste that is on the bristles against your teeth")
+        Console.WriteLine("Move the toothbrush back and forth horizontally on the teeth")
+        Console.WriteLine("Iterate motion over all the (exposed surfaces of the) teeth for two minutes")
+        Console.WriteLine("Brush the tongue until you gag")
+        Console.WriteLine("Remove toothbrush from your mouth")
+        Console.WriteLine("Rinse the toothpaste out of your mouth and into the LiquidWasteReceptacle")
+        Console.WriteLine("Wipe your mouth")
+        Console.WriteLine("Put the toothpaste away")
+        Console.WriteLine("Put the toothbrush away")
+        Console.WriteLine("Rinse the sink")
+        Console.WriteLine("Wipe the counter")
+
+
+
+
+
         Console.ReadKey()
     End Sub
 
