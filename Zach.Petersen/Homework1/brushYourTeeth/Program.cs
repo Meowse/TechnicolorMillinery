c8c2904bd5623e051603e410976ff85988108c86
Zach Petersen <zach@e53.org>
2014-07-01 10:15:32 -0700

    array out - list in
    trying a list for instructions instead of an array.

--- Zach.Petersen/Homework1/brushYourTeeth/Program.cs
+++ Zach.Petersen/Homework1/brushYourTeeth/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace brushYourTeeth
 {
+    class brushingInstruction
+    {
+        public int StepId;
+        public int RequiresTimer;
+        public int Duration;
+        public string Instruction;
+        public brushingInstruction(int StepId, 
+                                   int RequiresTimer, 
+                                   int Duration, 
+                                   string Instruction)
+        {
+            //constructor bodies for the class
+            this.StepId = id;
+            this.RequiresTimer = rt;
+            this.Duration = dur;
+            this.Instruction = ins;
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
@@ -21,34 +40,15 @@ namespace brushYourTeeth
             Console.Read();
            
             //tooth brushing process
-            BrushTeeth();
+            //populate instructions list
+            List<brushingInstruction> newBrushingInstructionList = new List<brushingInstruction>();
+
+
             Console.Read();
 
         }
 
-        //9 out of 10 dentists agree that this is the start of the brushing process
-        static void BrushTeeth()
-        {
-            var brushingSteps = new string[,] { //create and initialize array of instructions
-                //stepId,requiresTimer,duration,instruction
-                //{"","0","0",""},
-                {"1","0","0","Position yourself near the Liquid Waste Receptacle"},
-                {"2","0","0","Pick up and hold the toothpaste in your right hand"},
-                {"3","0","0","Remove the cap from the toothpaste container"},
-                {"4","0","0","Pick up and hold the toothbrush in your left hand positioning it so that the bristles are oriented upwards"},
-                {"5","0","0","Align the toothpaste container, dispensing end first, with one end of the bristles on the toothbrush"},
-                {"6","0","0","Gently apply pressure to the toothpaste container while simultaneously moving the toothpaste container along the top of the bristles so as to apply a single layer of toothpaste to the brush. Upon reaching the opposite end of the bristles immediately stop applying pressure to the toothpaste container"},
-                {"7","0","0","Place the toothpaste container back onto its storage surface with its cap reattached"},
-                {"8","0","0","Keeping the toothbrush with the toothpaste covered bristles oriented in an upward fashion move the toothbrush to your right hand"},
-                {"9","0","0","Open your mouth and position the toothbrush against your front teeth so that the toothpaste covered bristles are in direct contact with the surface of your front teeth"},
-                {"10","0","0","To begin brushing move the toothbrush in a vertical up and down motion on your front teeth"},
-                {"11","1","300","To complete the brushing process move the toothbrush over all exposed surfaces of each tooth in your mouth.  Each tooth should receive approximately the same amount of brushing effort. Continue this process for a total of two minutes. At the end of two minutes position the toothbrush bristles onto your tongue and move the toothbrush over the whole surface of your tongue for ten seconds"},
-                {"12","0","0","When the brushing process is completed remove the toothbrush from your mouth and position it over the liquid waste receptacle for cleaning"},
-                {"13","0","0","To clean the toothbrush rinse with a sufficient amount of water to clean all toothpaste residue from the bristles and handle of the toothbrush. Once cleaning has been completed place the toothbrush back in its storage container"},
-                {"14","1","5","Next open your mouth and dispense three ounces of water into your mouth without swallowing any of the water. Close your mouth and swish the water back and forth for five seconds to clean the toothpaste from inside your mouth. After five seconds has elapsed position your mouth over the liquid waste receptacle and discharge the water from your mouth into the liquid waste receptacle."},
-	        };
 
-
-        }
     }
 }
+
