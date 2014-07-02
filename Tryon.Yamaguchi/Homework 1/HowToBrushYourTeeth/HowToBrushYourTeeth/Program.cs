2ff010ddc177d7e80a7f0c07b510d0579a9d8a4d
tryon12 <tryon12@hotmail.com>
2014-07-01 21:59:26 -0700

    Extra Credit How To Brush Your Teeth

---
+++ Tryon.Yamaguchi/Homework 1/HowToBrushYourTeeth/HowToBrushYourTeeth/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToBrushYourTeeth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How " + "To " + "Brush " + "Your " + "Teeth");
+            Console.WriteLine("Step 1:  Pick up your toothpaste container with your non dominant hand.");
+            Console.WriteLine("Step 2:  Unscrew the cap of the toothpaste container with your dominant hand.");
+            Console.WriteLine("Step 3:  Place the cap down.");
+            Console.WriteLine("Step 4:  With your dominant hand, pick up your toothbrush by the handle with the bristols pointing up.");
+            Console.WriteLine("Step 5:  Gently squeeze the toothpaste out of the toothpaste container onto the bristols of your toothbrush until the toothpaste covers majorly top of the bristols.");
+            Console.WriteLine("Step 6:  Put the toothpaste container down.");
+            Console.WriteLine("Step 7:  Open your mouth.");
+            Console.WriteLine("Step 8:  Place toothpaste on the your tooth brush against your teeth.");
+            Console.WriteLine("Step 9:  Start moving toothbrush so the bristols brush against your teeth.");
+            Console.WriteLine("Step 10:  Iterate the motion over all the exposed surfaces of your teeth for next 2 minutes.");
+            Console.WriteLine("Step 11:  Move the bristols to the top surface of your tongue and start moving the bristols on the expose top surface of your tongue.");
+            Console.WriteLine("Step 12:  Remove the brush from your mouth.");
+            Console.WriteLine("Step 13:  Turn on the cold water on the sink.");
+            Console.WriteLine("Step 14:  Place the toothbrush bristols under the running water for 10 seconds.");
+            Console.WriteLine("Step 15:  Move the toothbrush back and forth under the running water for 5 seconds.");
+            Console.WriteLine("Step 16:  Put the toothbrush down.");
+            Console.WriteLine("Step 17:  Fill a cup half way with water.");
+            Console.WriteLine("Step 18:  Drink from the cup but do not swallow.");
+            Console.WriteLine("Step 19:  Keep your mouth closed and swoosh the water around your mouth.");
+            Console.WriteLine("Step 20:  Open your mouth slightly and spit the liquid out of your mouth into the sink.");
+            Console.WriteLine("Step 21:  Repeat the last 3 steps.");
+            Console.WriteLine("Step 22:  Turn off the cold water");
+            Console.WriteLine("Step 23:  Pick up the toothpaste container in one hand and toothpaste cap in the other.");
+            Console.WriteLine("Step 24:  Screw the cap back on the top of the toothpaste container's opening until it will not turn anymore.");
+            Console.WriteLine("Step 25:  Put down the toothpaste container.");
+
+            Console.ReadKey();
+        }
+    }
+}
