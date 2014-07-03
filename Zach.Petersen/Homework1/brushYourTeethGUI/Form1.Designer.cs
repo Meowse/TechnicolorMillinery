90d34bc12b6c2f11ac15f7a824bd338bd80cce64
Zach Petersen <zach@e53.org>
2014-07-03 16:28:59 -0700

    Form Fields and List
    Not functioning. Build errors. Cant link back to list from form. doh!

--- Zach.Petersen/Homework1/brushYourTeethGUI/Form1.Designer.cs
+++ Zach.Petersen/Homework1/brushYourTeethGUI/Form1.Designer.cs
@@ -29,31 +29,33 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 421);
+            this.button1.Location = new System.Drawing.Point(31, 384);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(97, 60);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "buttonBrush";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // textBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(210, 134);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(196, 22);
-            this.textBox1.TabIndex = 1;
+            this.textBox.Location = new System.Drawing.Point(374, 41);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(388, 204);
+            this.textBox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(893, 489);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(788, 489);
+            this.Controls.Add(this.textBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,7 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
