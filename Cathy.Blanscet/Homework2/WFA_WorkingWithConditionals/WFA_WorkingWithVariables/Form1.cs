250e40397c6b3db27d31e53a50afa5635b46163b
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-07-04 16:13:08 -0700

    working with variables part 1

---
+++ Cathy.Blanscet/Homework2/WFA_WorkingWithConditionals/WFA_WorkingWithVariables/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_WorkingWithVariables
+{   
+    public partial class Form1 : Form
+    {
+        int MyNum = 0;
+        int CondNum = 0;
+        string Msg1 = "";
+        string Msg2 = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GreetMeButtonClicked(object sender, EventArgs e)
+        {
+             string varName = NameTextBox.Text;
+            MessageBox.Show("Hello " + varName);
+
+        }
+
+        private void IncrementNumberButtonClicked(object sender, EventArgs e)
+        {
+            MyNum ++;
+            Msg1 = MyNum + "\nClick the button Click Me and watch what happens";
+            ClickMeLabel.Text = Msg1 = MyNum + "\nClick the button Click Me and watch what happens";
+            ;
+        }
+
+        private void ConditionalTestButtonClicked(object sender, EventArgs e)
+        {
+            CondNum++;
+            if (CondNum > 10)
+            {
+                CondNum = 0;
+                Msg2 = CondNum + "\nClicked too many times - Reset to 0\nClick the button Test Me!";
+                TestMeLabel.Text = Msg2;              
+               
+            }
+            else
+            {
+                Msg2 = CondNum + "\nClick the button Test Me!";
+                TestMeLabel.Text = Msg2;              
+            }
+            TestMeLabel.Text = Msg2;
+        }
+    }
+}
