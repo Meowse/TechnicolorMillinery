faa59fa96f83a9d5a953b3d927b2db835f0fbc6f
aodenwe <aaron.odenweller@gmail.com>
2014-07-06 14:25:31 -0700

    Modified Windows Form Header to "Hello World"

--- Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.Designer.cs
+++ Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.Controls.Add(this.btnHello);
             this.Controls.Add(this.lblHello);
             this.Name = "Form1";
-            this.Text = "Instructions";
+            this.Text = "Hello World";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
