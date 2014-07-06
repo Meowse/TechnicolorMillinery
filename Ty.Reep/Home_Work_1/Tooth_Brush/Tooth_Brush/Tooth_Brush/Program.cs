f7ddb3c61a87ac351b4eb6a77216022461127fc7
Tyreep <tyreep@tyberdude.com>
2014-07-06 12:34:48 -0700

    less write lines

--- Ty.Reep/Home_Work_1/Tooth_Brush/Tooth_Brush/Tooth_Brush/Program.cs
+++ Ty.Reep/Home_Work_1/Tooth_Brush/Tooth_Brush/Tooth_Brush/Program.cs
@@ -9,30 +9,31 @@ namespace Tooth_Brush
     class Program
     {
         static void Main(string[] args)
-        {Console.WriteLine("How to brush your teeth");
-        Console.WriteLine("Tell Teenage to go apply War Paint Some Where Else");
-        Console.WriteLine("Recive Dirty Look from Teenager");
-       Console.WriteLine("Acquire LiquidWasteReceptacle");
-    Console.WriteLine("Acquire a toothbrush"); 
-    Console.WriteLine("Apply toothpaste to the toothbrush we just acquired");
-            Console.WriteLine("Acquire toothpaste tube");
-            Console.WriteLine("Pick up your toothbrush");
-            Console.WriteLine("Pick up your toothpaste");
-            Console.WriteLine("Unscrew the top with one hand");
-            Console.WriteLine("Align the bristles with the opening of the tube");
-            Console.WriteLine("Make sure the bristles are pointing up");
-            Console.WriteLine("Make sure that the toothbrush is below the toothpaste");
-            Console.WriteLine("Gently squeeze a small amount of the toothpaste onto the toothbrush");
-            Console.WriteLine("small -- the same size as the length and width of the bristles");
-            Console.WriteLine("Put the cap back on the toothpaste");
-                Console.WriteLine("Put the toothpaste tube down");
-    Console.WriteLine("Open mouth and Place the toothpaste that is on the bristles against your teeth");
-    Console.WriteLine("Move the toothbrush back and forth horizontally on the teeth");
-    Console.WriteLine("Iterate motion over all the (exposed surfaces of the) teeth for two minutes");
-    Console.WriteLine("Brush the tongue until you gag");
-    Console.WriteLine("Remove toothbrush from your mouth");
-    Console.WriteLine("Rinse the toothpaste out of your mouth and into the LiquidWasteReceptacle");
-    Console.ReadKey();
+        {
+            Console.WriteLine("How to brush your teeth");
+            Console.WriteLine("Tell Teenage to go apply War Paint Some Where Else\n" +
+            "Recive Dirty Look from Teenager\n" +
+            "Acquire LiquidWasteReceptacle");
+            Console.WriteLine("Acquire a toothbrush\n" +
+            "Apply toothpaste to the toothbrush we just acquired\n" +
+            "Acquire toothpaste tube");
+            Console.WriteLine("Pick up your toothbrush\n" +
+            "Pick up your toothpaste\n" +
+            "Unscrew the top with one hand");
+            Console.WriteLine("Align the bristles with the opening of the tube\n" +
+            "Make sure the bristles are pointing up");
+            Console.WriteLine("Make sure that the toothbrush is below the toothpaste\n" +
+            "Gently squeeze a small amount of the toothpaste onto the toothbrush");
+            Console.WriteLine("small -- the same size as the length and width of the bristles\n" +
+            "Put the cap back on the toothpaste\n" +
+            "Put the toothpaste tube down");
+            Console.WriteLine("Open mouth and Place the toothpaste that is on the bristles against your teeth\n" +
+            "Move the toothbrush back and forth horizontally on the teeth");
+            Console.WriteLine("Iterate motion over all the (exposed surfaces of the) teeth for two minutes\n" +
+            "Brush the tongue until you gag");
+            Console.WriteLine("Remove toothbrush from your mouth\n" +
+                "Rinse the toothpaste out of your mouth and into the LiquidWasteReceptacle");
+            Console.ReadKey();
         }
     }
 }
