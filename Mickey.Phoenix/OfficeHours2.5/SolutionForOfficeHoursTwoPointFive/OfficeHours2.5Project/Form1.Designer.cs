d6831c6791bb2cb44bfa62a652bd75a07845009d
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-07-06 16:13:24 -0700

    First commit for Office Hours 2.5

---
+++ Mickey.Phoenix/OfficeHours2.5/SolutionForOfficeHoursTwoPointFive/OfficeHours2.5Project/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace OfficeHours2._5Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openNamedItRightButton = new System.Windows.Forms.Button();
+            this.otherGoodNameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openNamedItRightButton
+            // 
+            this.openNamedItRightButton.Location = new System.Drawing.Point(13, 13);
+            this.openNamedItRightButton.Name = "openNamedItRightButton";
+            this.openNamedItRightButton.Size = new System.Drawing.Size(157, 36);
+            this.openNamedItRightButton.TabIndex = 0;
+            this.openNamedItRightButton.Text = "Named it right!";
+            this.openNamedItRightButton.UseVisualStyleBackColor = true;
+            // 
+            // otherGoodNameButton
+            // 
+            this.otherGoodNameButton.Location = new System.Drawing.Point(13, 55);
+            this.otherGoodNameButton.Name = "otherGoodNameButton";
+            this.otherGoodNameButton.Size = new System.Drawing.Size(157, 36);
+            this.otherGoodNameButton.TabIndex = 1;
+            this.otherGoodNameButton.Text = "Good name!";
+            this.otherGoodNameButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 398);
+            this.Controls.Add(this.otherGoodNameButton);
+            this.Controls.Add(this.openNamedItRightButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openNamedItRightButton;
+        private System.Windows.Forms.Button otherGoodNameButton;
+    }
+}
+
