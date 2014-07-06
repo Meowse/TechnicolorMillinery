2b00dabf97376bc6580281f2cff0bd4e439ae876
aodenwe <aaron.odenweller@gmail.com>
2014-07-06 14:20:42 -0700

    HW 2:  HW1's Extra Credit
    The contents of this commit contains my windows forms application and
    console application per the requirements of Homework 1's extra credit

---
+++ Aaron.Odenweller/Homework2/ExtraCreditHW1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Aaron\Documents\GitHub\TechnicolorMillinery\Aaron.Odenweller\Homework1\WindowsFormsApplicationHW1\WindowsFormsApplicationHW1\How to brush your teeth.txt");
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
