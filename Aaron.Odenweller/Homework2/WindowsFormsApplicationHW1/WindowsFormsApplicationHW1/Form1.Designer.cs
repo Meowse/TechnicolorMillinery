8a0043e0aaa1963bc7f4587c7df0af4ca98b6659
aodenwe <aaron.odenweller@gmail.com>
2014-07-06 15:13:14 -0700

    Form with "Don't Push Me" Button and atomic bomb

--- Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.Designer.cs
+++ Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.Designer.cs
@@ -28,15 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblHello = new System.Windows.Forms.Label();
             this.btnHello = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblBomb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblHello
             // 
             this.lblHello.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblHello.AutoSize = true;
-            this.lblHello.Location = new System.Drawing.Point(99, 28);
+            this.lblHello.Location = new System.Drawing.Point(101, 28);
             this.lblHello.Name = "lblHello";
             this.lblHello.Size = new System.Drawing.Size(0, 13);
             this.lblHello.TabIndex = 0;
@@ -45,7 +50,7 @@
             // 
             this.btnHello.AllowDrop = true;
             this.btnHello.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btnHello.Location = new System.Drawing.Point(247, 332);
+            this.btnHello.Location = new System.Drawing.Point(127, 332);
             this.btnHello.Name = "btnHello";
             this.btnHello.Size = new System.Drawing.Size(75, 23);
             this.btnHello.TabIndex = 1;
@@ -53,16 +58,55 @@
             this.btnHello.UseVisualStyleBackColor = true;
             this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
             // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(288, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Don\'t Push Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 298);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblBomb
+            // 
+            this.lblBomb.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblBomb.AutoSize = true;
+            this.lblBomb.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBomb.ForeColor = System.Drawing.Color.Red;
+            this.lblBomb.Location = new System.Drawing.Point(69, 369);
+            this.lblBomb.Name = "lblBomb";
+            this.lblBomb.Size = new System.Drawing.Size(0, 39);
+            this.lblBomb.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 383);
+            this.ClientSize = new System.Drawing.Size(601, 429);
+            this.Controls.Add(this.lblBomb);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnHello);
             this.Controls.Add(this.lblHello);
             this.Name = "Form1";
             this.Text = "Hello World";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +116,9 @@
 
         private System.Windows.Forms.Label lblHello;
         private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblBomb;
     }
 }
 
--- Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.cs
+++ Aaron.Odenweller/Homework2/WindowsFormsApplicationHW1/WindowsFormsApplicationHW1/Form1.cs
@@ -15,16 +15,38 @@ namespace WindowsFormsApplicationHW1
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Visible = false;
+            lblBomb.Text = "Now You've Done It!!!";
+            lblBomb.Visible = false;
+            
         }
 
         private void btnHello_Click(object sender, EventArgs e)
         {
+            Text = "Hello World";
+            lblBomb.Visible = false;
+            lblHello.Visible = true;
+            pictureBox1.Visible = false;
             string text = System.IO.File.ReadAllText(@"C:\Users\Aaron\Documents\GitHub\TechnicolorMillinery\Aaron.Odenweller\Homework1\WindowsFormsApplicationHW1\WindowsFormsApplicationHW1\How to brush your teeth.txt");
             lblHello.Text = text;
            
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text = "Goodbye World";
+            lblBomb.Visible = true;
+            lblHello.Visible = false;
+            pictureBox1.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
