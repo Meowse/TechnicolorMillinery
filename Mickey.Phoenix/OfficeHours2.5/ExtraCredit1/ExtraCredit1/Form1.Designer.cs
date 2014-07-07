c22642e3bad8a476276e7747976ab33ba7db21d2
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-07-06 17:13:49 -0700

    Extra credit for Homework 2

---
+++ Mickey.Phoenix/OfficeHours2.5/ExtraCredit1/ExtraCredit1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ExtraCredit1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.greetingsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(56, 30);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(100, 20);
+            this.nameField.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Greet Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GreetThem);
+            // 
+            // greetingsLabel
+            // 
+            this.greetingsLabel.AutoSize = true;
+            this.greetingsLabel.Location = new System.Drawing.Point(243, 33);
+            this.greetingsLabel.Name = "greetingsLabel";
+            this.greetingsLabel.Size = new System.Drawing.Size(97, 13);
+            this.greetingsLabel.TabIndex = 2;
+            this.greetingsLabel.Text = "show greeting here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hello!  Please enter your name and click \"Greet Me!\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 126);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.greetingsLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nameField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label greetingsLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Mickey.Phoenix/OfficeHours2.5/ExtraCredit1/ExtraCredit1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtraCredit1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            greetingsLabel.Text = "";
+        }
+
+        private void GreetThem(object sender, EventArgs e)
+        {
+            String userName = nameField.Text;
+            if (userName.Length == 0)
+            {
+                MessageBox.Show("Please enter your name, so I can greet you properly!");
+            }
+            else
+            {
+                String greeting = "Hello, " + userName;
+                greetingsLabel.Text = greeting;
+            }
+        }
+
+        // Options:
+        // 1) Display a message box saying "Please enter your name."
+        // 2) Have it say "Hello" without a name.
+        // 3) Have it say "Hello!  What's your name?"
+        // 4) Add instructions, and then display the error message box.
+
+        // Entering a phone number:
+        // 1) Give an example.  E.g.     "Phone Number [                 ]  (xxx-xxx-xxxx)
+        // 2) Accept anything they give you that has ten digits and no letters, e.g. (nnn) nnn-nnnn, nnn nnn nnnn, nnn-nnn-nnnn
+        // 3) Re-display it in a standard, "canonical" format (e.g. nnn-nnn-nnnn or (nnn) nnn-nnnn)
+
+        // Especially important with dates!
+        // What is this date?  2/1/2014
+        // Let them enter 2/1/2014, but re-display it as 2014-02-01 (or, even better, "Feb. 01, 2014" if the user will
+        // put up with that)
+        // 2/1/2014
+        // 2014-02-01
+        // Feb. 1, 2014 -- no way for the mistake to be un-noticed.
+        // 2/1/2014
+        // You can always display: 2/1/2014 (February 1, 2014)
+    }
+}
