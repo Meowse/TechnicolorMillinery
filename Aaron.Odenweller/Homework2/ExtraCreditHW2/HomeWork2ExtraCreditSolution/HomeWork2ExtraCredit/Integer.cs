4a01a0650233aff62a601ebe9b36686ca598e48f
aodenwe <aaron.odenweller@gmail.com>
2014-07-08 09:52:53 -0700

    Homework 2 Extra Credit
    Windows Form with a custom Greeting class, which takes input from text
    box and produces output, and a custom Integer class which adds 1 for
    every button click

---
+++ Aaron.Odenweller/Homework2/ExtraCreditHW2/HomeWork2ExtraCreditSolution/HomeWork2ExtraCredit/Integer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2ExtraCredit
+{
+    public class Integer
+    {
+        public static Int16 num;
+        public Int16 aNum;
+
+        public Integer()
+        {
+            addInteger();
+            aNum = num;                
+        }
+
+        private void addInteger(){
+            num++;
+        }
+
+        public void setInteger()
+        {
+            num = 0;
+            aNum = num;
+        }
+    }
+}
