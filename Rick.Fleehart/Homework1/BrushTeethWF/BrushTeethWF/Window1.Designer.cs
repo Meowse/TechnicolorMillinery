b3b3a19732840d1131df340a455e5505af257dcd
rickfleehart <rickfleehart@gmail.com>
2014-07-07 21:02:40 -0700

    Almost final code
    Windows form with a variable to hold the text in a message box.

---
+++ Rick.Fleehart/Homework1/BrushTeethWF/BrushTeethWF/Window1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BrushTeethWF
+{
+    partial class Window1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartProcessbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Teeth Brushing Details";
+            // 
+            // StartProcessbutton
+            // 
+            this.StartProcessbutton.Location = new System.Drawing.Point(186, 36);
+            this.StartProcessbutton.Name = "StartProcessbutton";
+            this.StartProcessbutton.Size = new System.Drawing.Size(158, 23);
+            this.StartProcessbutton.TabIndex = 1;
+            this.StartProcessbutton.Text = "Press to show the process.";
+            this.StartProcessbutton.UseVisualStyleBackColor = true;
+            this.StartProcessbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 409);
+            this.Controls.Add(this.StartProcessbutton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Welcome to Teeth Brushing for You";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartProcessbutton;
+    }
+}
+
