3a99afaaf8452a6d040fc48b3efe6a30c63ca0f9
lammersb <bj_lammers@msn.com>
2014-07-07 18:27:21 -0700

    HelloWorldForm - Homework2 baseline
    HelloWorldForm - Homework2 baseline

---
+++ Bridget.Lammers/Homework2/HelloWorldForm/HelloWorldForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Bridget");
+
+        }
+
+        private void howtobrushteeth_button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("How to brush your teeth at a sink\n\nGo to a sink\nGet a toothbrush\nGet your favorite toothpaste\nApply toothpaste to the toothbrush\nFirst hold the toothbrush in your right hand\nHold toothpaste in left hand\nWith the bristles facing upward to the sky\nSqueeze a small amount onto the toothbrush, a little more than a size of a pea\n   turn on the water in your sink\nget the toothbrush with the toothpaste a little wet\n\nPut the toothbrush with toothpaste in your mouth\nGo up and down with your toothbrush\nstart at the upper gum and work your way down the lower gum");
+
+        }
+    }
+}
