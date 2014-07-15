9e3158efe8e9f04f1687d5859a20794bf2f59b64
davidswa08 <dswartz@uw.edu>
2014-07-15 13:49:00 -0700

    Homework 1

---
+++ Dave.Swartzendruber/Tooth_Brush_Instruction/Tooth_Brush_Instruction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tooth_Brush_Instruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World" /n "test");
+            Console.WriteLine("Press any Key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
