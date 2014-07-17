713b60e4deffa0b8b4269010016de2278c26ad83
RichKenn <RichKenn@myuw.net>
2014-07-16 19:55:11 -0700

    Homework 2 Credit 3 Final

---
+++ Richard.Kennedy/Homework2/GreetMeMicley/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreetMeMicley
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                // Check if a name was entered
+                if (textBox1.Text == "")
+                {
+                    // Clear prior name
+                    label2.Text = "";
+                    // Give warning
+                    MessageBox.Show("Please enter your first name.");
+                }
+                else
+                {
+                    if (textBox1.Text == "Mickey")
+                    {
+                        label2.Text = "Hello, Teacher!";
+                    }
+                    else
+                    {
+                        label2.Text = "Hello, " + textBox1.Text;
+                    }
+                }
+
+            }
+        }
+    }
+}
