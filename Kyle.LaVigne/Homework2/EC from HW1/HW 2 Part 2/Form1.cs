9486cdde8807bdd99a5e0aae364e52e64ccea06f
kdlavigne <kdlv@uw.edu>
2014-07-16 19:13:01 -0700

    HW 2

---
+++ Kyle.LaVigne/Homework2/EC from HW1/HW 2 Part 2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_2_Part_2
+{
+    public partial class BrushInstructions : Form
+    {
+        public BrushInstructions()
+        {
+            InitializeComponent();
+        }
+
+        private void BrushTeeth(object sender, EventArgs e)
+        {
+            MessageBox.Show("Instructions\n"
+    +"\n"
+    + "1. Acquire Materials, including:\n"
+	+ "     -LiquidWasteReceptacle -- details TBD\n"
+	+ "     -Toothbrush	-- details TBD\n"
+	+ "     -Toothpaste tube -- details TBD\n"
+    + "\n"
++ "2. Properly set up materials for brushing:\n"
+	+ "     -Pick up your toothpaste tube with your hands\n"
+	+ "     -Unscrew the top\n"
+    + "     -Place cap on flat surface nearby, preferably LiquidWasteReceptacle\n"
+	+ "     -Hold toothpaste tube with your preferred hand\n"
+	+ "     -Pick up toothbrush with free hand\n"
+	+ "     -Align the toothbrush bristles with the opening of the tube\n"
+	+ "     -Make sure the bristles are pointing up\n"
+	+ "     -Make sure that the toothbrush is below the toothpaste\n"
+	+ "     -Gently squeeze an amount (roughly equal to fingertip size) of toothpaste onto\n"
+    + "         the toothbrush\n"
+	+ "     -Put the toothpaste tube down\n"
+    + "\n"
++ "3. The Brushing\n"
+    + "     -Open mouth\n"
+	+ "     -Carefully draw the toothbrush into your mouth\n"
+	+ "     -Place the toothpaste that is on the bristles against your teeth\n"
+	+ "     -Move the toothbrush in a gentle circular motion across the teeth\n"
+	+ "     -Iterate motion over all the (exposed surfaces of the) teeth for two minutes\n"
+	+ "     -Brush the tongue for your preferred amount of time, not to exceed 10 seconds\n" 
+	+ "     -Remove toothbrush from your mouth\n"
+    + "\n"
++ "4.  Cleaning up\n"
+    +"      -Rinse the toothpaste out of your mouth and into the LiquidWasteReceptacle\n"        
+    +"      -Run the bristles of the toothbrush under water from faucet in\n"
+    +"          LiquidWastereceptacle to clean toothbrush\n"
+    +"      -Tighten cap back on toothpaste tube\n"     
+    +"      -Pack materials (toothpaste tube and toothbrush) back to proper destination\n", 
+               "How to Brush Your Teeth");
+        }
+
+        private void ExpertStatus(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hope the tutorial was informative. Happy brushing!" , "Congratulations");
+        }
+    }
+}
---
+++ Kyle.LaVigne/Homework2/greetings/HW 2 EC Part 1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace HW_2_EC_Part_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.GreetingLabel = new System.Windows.Forms.Label();
+            this.GreetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(94, 13);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(100, 20);
+            this.nameField.TabIndex = 1;
+            // 
+            // GreetingLabel
+            // 
+            this.GreetingLabel.AutoSize = true;
+            this.GreetingLabel.Location = new System.Drawing.Point(133, 93);
+            this.GreetingLabel.Name = "GreetingLabel";
+            this.GreetingLabel.Size = new System.Drawing.Size(76, 13);
+            this.GreetingLabel.TabIndex = 2;
+            this.GreetingLabel.Text = "                       ";
+            // 
+            // GreetButton
+            // 
+            this.GreetButton.Location = new System.Drawing.Point(26, 79);
+            this.GreetButton.Name = "GreetButton";
+            this.GreetButton.Size = new System.Drawing.Size(85, 40);
+            this.GreetButton.TabIndex = 3;
+            this.GreetButton.Text = "Greet Me!";
+            this.GreetButton.UseVisualStyleBackColor = true;
+            this.GreetButton.Click += new System.EventHandler(this.GreetButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.GreetButton);
+            this.Controls.Add(this.GreetingLabel);
+            this.Controls.Add(this.nameField);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Greetings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Label GreetingLabel;
+        private System.Windows.Forms.Button GreetButton;
+    }
+}
+
