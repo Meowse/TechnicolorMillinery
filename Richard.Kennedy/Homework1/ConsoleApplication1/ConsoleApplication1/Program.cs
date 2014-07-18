1529b2007d8e8dfccc66299d9b3423fb5f2db30e
RichKenn <RichKenn@myuw.net>
2014-07-17 18:56:29 -0700

    Uncommitted from ConsoleApplication1

---
+++ Richard.Kennedy/Homework1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean isCorrect = true;
+            if (isCorrect) {
+                Console.WriteLine("Good job!");
+            } else {
+                Console.WriteLine("Try again!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
