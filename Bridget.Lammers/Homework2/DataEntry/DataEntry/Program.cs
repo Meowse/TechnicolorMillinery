13fcd9679f2622b7de6e1527269236676b02ef74
lammersb <bj_lammers@msn.com>
2014-07-17 20:34:04 -0700

    Inclass work homework 2
    inclass work homework2

---
+++ Bridget.Lammers/Homework2/DataEntry/DataEntry/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your age");
+        }
+    }
+}
