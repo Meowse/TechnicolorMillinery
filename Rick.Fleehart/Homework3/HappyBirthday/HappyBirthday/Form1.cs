a0f8060988f4ed698fa6f33369c737d09ddc90a2
rickfleehart <rickfleehart@yahoo.com>
2014-07-19 09:45:24 -0700

    Homework3 Happy Bday Initial Commit
    Windows form with all the labels and part but little functioning.

---
+++ Rick.Fleehart/Homework3/HappyBirthday/HappyBirthday/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HappyBirthday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            SelectedDate.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
+        }
+
+        // if today is birthday, display Happy birthday song
+
+        // if today is not birthday but morning display good morning
+ 
+        // if today is not birthday but noon to 5pm display is taking a siesta
+
+        // if today is not birthday but after 5PM then display lullaby
+
+
+
+        //------------------ old code home -----------------------------
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //This should not be here but if I remove it everything explodes
+        }
+    }
+}
