5dc3af1326fcf3ce3d25111e2b5d2476cb835f86
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-07-19 09:54:40 -0700

    Happy B'Day and EC with Age

--- Cathy.Blanscet/Homework3/HappyBdayExtra/HappyBdayExtra/Form1.Designer.cs
+++ Cathy.Blanscet/Homework3/HappyBdayExtra/HappyBdayExtra/Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.BDayBox = new System.Windows.Forms.DateTimePicker();
             this.BDayLabel = new System.Windows.Forms.Label();
             this.ResultLabel = new System.Windows.Forms.Label();
+            this.BirthYearTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ClickMeButton
             // 
-            this.ClickMeButton.Location = new System.Drawing.Point(15, 52);
+            this.ClickMeButton.Location = new System.Drawing.Point(15, 38);
             this.ClickMeButton.Name = "ClickMeButton";
             this.ClickMeButton.Size = new System.Drawing.Size(75, 23);
             this.ClickMeButton.TabIndex = 0;
@@ -82,17 +84,35 @@
             // ResultLabel
             // 
             this.ResultLabel.AutoSize = true;
-            this.ResultLabel.Location = new System.Drawing.Point(12, 87);
+            this.ResultLabel.Location = new System.Drawing.Point(12, 73);
             this.ResultLabel.Name = "ResultLabel";
             this.ResultLabel.Size = new System.Drawing.Size(20, 17);
             this.ResultLabel.TabIndex = 5;
             this.ResultLabel.Text = "   ";
             // 
+            // BirthYearTextBox
+            // 
+            this.BirthYearTextBox.Location = new System.Drawing.Point(828, 18);
+            this.BirthYearTextBox.Name = "BirthYearTextBox";
+            this.BirthYearTextBox.Size = new System.Drawing.Size(100, 22);
+            this.BirthYearTextBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(621, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter the year you were born:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 450);
+            this.ClientSize = new System.Drawing.Size(1077, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BirthYearTextBox);
             this.Controls.Add(this.ResultLabel);
             this.Controls.Add(this.BDayLabel);
             this.Controls.Add(this.BDayBox);
@@ -114,6 +134,8 @@
         private System.Windows.Forms.DateTimePicker BDayBox;
         private System.Windows.Forms.Label BDayLabel;
         private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.TextBox BirthYearTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Cathy.Blanscet/Homework3/HappyBdayExtra/HappyBdayExtra/Form1.cs
+++ Cathy.Blanscet/Homework3/HappyBdayExtra/HappyBdayExtra/Form1.cs
@@ -21,87 +21,127 @@ namespace HappyBdayExtra
         {
             string varName = NameTextBox.Text;
             int varGoodName = 0;
+
             string varBDate = BDayBox.Text;
             string varSysDate = DateTime.Now.ToString("M/d/yyyy");
 
-            /*string varBDline1 = "Happy Birthday to you.";
-            string varBDline2 = "Happy Birthday, Dear ";
-            string varGMline1 = "Good Morning to you.";
-            string varGMline2 = "Good Morning, Dear ";*/
+            // for part b of Extra Credit
+            int varCurrentYear = DateTime.Now.Year;
+            var varBirthYear = BirthYearTextBox.Text;
+            int BirthYr;
+            bool varIsGoodYear = int.TryParse(varBirthYear, out BirthYr);
 
-            string varSiestaTime = "We're sorry but it is siesta time between noon and 5 pm.";
+            if (varIsGoodYear)
+            {
+                int age = (varCurrentYear - BirthYr);
+                
+                /*string varBDline1 = "Happy Birthday to you.";
+                string varBDline2 = "Happy Birthday, Dear ";
+                string varGMline1 = "Good Morning to you.";
+                string varGMline2 = "Good Morning, Dear ";*/
 
-            if (varName == "")
-            {
-                MessageBox.Show("I didn't catch your name.\nPlease enter your name.");
-            }
-            else
-            {
-                varGoodName = 1;
-            }
-            if (varGoodName > 0)
-            {
-                if (DateTime.Parse(varBDate) == DateTime.Parse(varSysDate))
+                string varSiestaTime = "We're sorry but it is siesta time between noon and 5 pm.";
+
+                if (varName == "")
                 {
-                    // replace this with a while loop
-                    //ResultLabel.Text = varBDline1 + "\n" + varBDline1 + "\n" + varBDline2 + varName + ",\n" + varBDline1;
-                    int cnt = 1;
-                    int maxCnt = 5;
-                    ResultLabel.Text = "";
-                    while (cnt < maxCnt)
-                    {
-                        if (cnt == 3)
-                        {
-                            ResultLabel.Text += "Happy Birthday, Dear " + varName + ",\n";
-                        }
-                        else
-                        {
-                            ResultLabel.Text += "Happy Birthday to you.\n";
-                        }
-                        cnt++;
-                    }
+                    MessageBox.Show("I didn't catch your name.\nPlease enter your name.");
                 }
                 else
                 {
-                   
-                    TimeSpan start = new TimeSpan(12, 0, 0);    //10 o'clock
-                    TimeSpan end = new TimeSpan(17, 0, 0);      //5 o'clock
-                    TimeSpan now = DateTime.Now.TimeOfDay;
-                    //MessageBox.Show("now is " + now);
-                    //MessageBox.Show("end is " + end);
+                    varGoodName = 1;
+                }
 
-                    if ((now < start)) //before noon
+                if (varGoodName > 0)
+                {
+                    if (DateTime.Parse(varBDate) == DateTime.Parse(varSysDate))
                     {
-                        //replace with a while loop
-                        //ResultLabel.Text = varGMline1 + "\n" + varGMline1 + "\n" + varGMline2 + varName + ",\n" + varGMline1;
+                        // replace this with a while loop
+                        //ResultLabel.Text = varBDline1 + "\n" + varBDline1 + "\n" + varBDline2 + varName + ",\n" + varBDline1;
                         int cnt = 1;
                         int maxCnt = 5;
-                        ResultLabel.Text = "";
-                        while (cnt < maxCnt)
+                        int maxLoop = 0;
+                        if (age == 0)
                         {
-                            if (cnt == 3)
+                            age = 1;
+                        }
+
+                        ResultLabel.Text = ""; // clear the previous text
+                        while (maxLoop < age)
+                        {
+                            while (cnt < maxCnt)
                             {
-                                ResultLabel.Text += "Good Morning, Dear " + varName + ",\n";
-                            }
-                            else
+                                if (cnt == 3)
+                                {
+                                    ResultLabel.Text += "Happy Birthday, Dear " + varName + ",\n";
+                                }
+                                else
+                                {
+                                    ResultLabel.Text += "Happy Birthday to you.\n";
+                                }
+                                cnt++;
+                            } 
+                            ResultLabel.Text += "\n";   //break between the versions
+                            cnt = 1;
+                            maxLoop ++;
+                        } 
+                    }
+                    else
+                    {
+                        TimeSpan start = new TimeSpan(12, 0, 0); //12 o'clock
+                        TimeSpan end = new TimeSpan(17, 0, 0); //5 o'clock
+                        TimeSpan now = DateTime.Now.TimeOfDay;
+                        //MessageBox.Show("now is " + now);
+                        //MessageBox.Show("end is " + end);
+
+                        if ((now < start)) //before noon
+                        {
+                            //replace with a while loop
+                            //ResultLabel.Text = varGMline1 + "\n" + varGMline1 + "\n" + varGMline2 + varName + ",\n" + varGMline1;
+                            int cnt = 1;
+                            int maxCnt = 5;
+                            int maxLoop = 0;
+                            if (age == 0)
                             {
-                                ResultLabel.Text += "Good Morning to you.\n";
+                                age = 1;
                             }
-                            cnt++;
+
+                            ResultLabel.Text = "";
+                            while (maxLoop < age)
+                            {
+                                while (cnt < maxCnt)
+                                {
+                                    if (cnt == 3)
+                                    {
+                                        ResultLabel.Text += "Good Morning, Dear " + varName + ",\n";
+                                    }
+                                    else
+                                    {
+                                        ResultLabel.Text += "Good Morning to you.\n";
+                                    }
+                                    cnt++;
+                                }
+                                ResultLabel.Text += "\n";   //break between the versions
+                                cnt = 1;
+                                maxLoop++;
+                            }
+                        }
+
+                        if ((now > start) && (now < end)) // between noon and 5 p.m.
+                        {
+                            ResultLabel.Text = varSiestaTime;
+                        }
+
+                        if ((now > end)) // after 5 p.m.
+                        {
+                            ResultLabel.Text =
+                                "Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you are!";
                         }
                     }
-
-                    if ((now > start) && (now < end))// between noon and 5 p.m.
-                    {
-                        ResultLabel.Text = varSiestaTime;
-                    }
-
-                    if ((now > end)) // after 5 p.m.
-                    {
-                        ResultLabel.Text = "Twinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.\nTwinkle, twinkle, little star,\nHow I wonder what you are!";
-                    }
-
-                }
+                } //end of varGoodName > 0
+            }
+            else
+            {
+                MessageBox.Show("I cannot calculate your age.\nPlease reenter the year of your birth in yyyy format.");
             }
         }
     }
