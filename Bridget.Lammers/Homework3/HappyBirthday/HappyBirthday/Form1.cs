3c8b5bf92ebab5003c4d4401945360a49046d2f4
lammersb <bj_lammers@msn.com>
2014-07-19 12:24:01 -0700

    Homework 3 happy birthday changes

--- Bridget.Lammers/Homework3/HappyBirthday/HappyBirthday/Form1.cs
+++ Bridget.Lammers/Homework3/HappyBirthday/HappyBirthday/Form1.cs
@@ -33,14 +33,34 @@ namespace HappyBirthday
 
             if (your_birthday_textbox.Text != null)
             {
+                // If you have a specific format for the string, you should use the other method:                         
+                // "d" stands for the short date pattern (see MSDN for more info) and null specifies that the current culture should be used for parsing the string.
+                //DateTime birthday_dt = DateTime.ParseExact(your_birthday_textbox.Text, "d", null);
+               // MessageBox.Show("Birthday Date " + birthday_dt);
+
                 DateTime birthday_dt = ConvertDate(your_birthday_textbox.Text, "MM/dd/yyyy");
 
+                //string[] bday_format = { "yyyyMMdd" };
+                //DateTime birthday_dt;
+
+                //if (DateTime.TryParseExact(your_birthday_textbox.Text,
+                  //                         bday_format,
+                   //                        System.Globalization.CultureInfo.InvariantCulture,
+                  //                         System.Globalization.DateTimeStyles.None,
+                  //                         out birthday_dt))
+                //{
+                    //valid
+               // }
+
                 DateTime todays_dt = DateTime.Now;
+                MessageBox.Show("Todays Date " + todays_dt);
+
                 string format = "MM/dd/yyyy";    // Use this format
                 string str_todays_dt = todays_dt.ToString(format);
-                MessageBox.Show("Todays Date " + str_todays_dt); 
+
+                MessageBox.Show("Todays Date " + str_todays_dt);
                 MessageBox.Show("Birthday Date " + birthday_dt);
-                 
+             
 
                 if (birthday_dt.Date > todays_dt.Date)
                 {
@@ -60,17 +80,22 @@ namespace HappyBirthday
             your_birthday_textbox.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
         }
 
-        public static DateTime ConvertDate(string date, string pattern)
+        public static DateTime ConvertDate(string in_date, string pattern)
         {
             MessageBox.Show("pattern " + pattern);
-            DateTime retval = DateTime.MinValue;
+            DateTime retval = DateTime.MinValue; //Represents the smallest possible value of DateTime. This field is read-only.
 
-            if (System.DateTime.TryParseExact(date, pattern, CultureInfo.InvariantCulture,
-                DateTimeStyles.None, out retval))
+            if (System.DateTime.TryParseExact(in_date, 
+                pattern, 
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None, 
+                out retval))
                 return retval;
 
             // Could not convert date..
             return DateTime.MinValue;
         }
+
+        
     }
 }
