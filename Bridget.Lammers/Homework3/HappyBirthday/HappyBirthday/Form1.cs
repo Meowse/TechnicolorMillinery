7d61ffc7410d77543884036bda5ff7dd429c0048
lammersb <bj_lammers@msn.com>
2014-07-19 19:02:08 -0700

    Homework 3 Happy Birthday #2 complete
    Homework 3 Happy Birthday #2 complete

--- Bridget.Lammers/Homework3/HappyBirthday/HappyBirthday/Form1.cs
+++ Bridget.Lammers/Homework3/HappyBirthday/HappyBirthday/Form1.cs
@@ -29,73 +29,67 @@ namespace HappyBirthday
             else if (your_birthday_textbox.Text == "")
             {
                 MessageBox.Show("Please enter a date, pick a date from the Date Picker, required field");
-            }
+            } //  End IF
 
             if (your_birthday_textbox.Text != null)
             {
-                // If you have a specific format for the string, you should use the other method:                         
-                // "d" stands for the short date pattern (see MSDN for more info) and null specifies that the current culture should be used for parsing the string.
-                //DateTime birthday_dt = DateTime.ParseExact(your_birthday_textbox.Text, "d", null);
-               // MessageBox.Show("Birthday Date " + birthday_dt);
-
-                DateTime birthday_dt = ConvertDate(your_birthday_textbox.Text, "MM/dd/yyyy");
-
-                //string[] bday_format = { "yyyyMMdd" };
-                //DateTime birthday_dt;
-
-                //if (DateTime.TryParseExact(your_birthday_textbox.Text,
-                  //                         bday_format,
-                   //                        System.Globalization.CultureInfo.InvariantCulture,
-                  //                         System.Globalization.DateTimeStyles.None,
-                  //                         out birthday_dt))
-                //{
-                    //valid
-               // }
-
+                // Converts the string representation of a date and time to its DateTime equivalent.  
+                DateTime birthday_dt = DateTime.Parse(your_birthday_textbox.Text);
+                
+                //  Get Todays Date
                 DateTime todays_dt = DateTime.Now;
-                MessageBox.Show("Todays Date " + todays_dt);
 
-                string format = "MM/dd/yyyy";    // Use this format
-                string str_todays_dt = todays_dt.ToString(format);
+                //  Get times of day
+                DateTime beforeNoon = new DateTime(todays_dt.Year, todays_dt.Month, todays_dt.Day, 11, 59, 59);
+                DateTime beforeFive = new DateTime(todays_dt.Year, todays_dt.Month, todays_dt.Day, 16, 59, 59);
+                DateTime afterFive = new DateTime(todays_dt.Year, todays_dt.Month, todays_dt.Day, 17, 0, 0);
 
-                MessageBox.Show("Todays Date " + str_todays_dt);
-                MessageBox.Show("Birthday Date " + birthday_dt);
-             
 
-                if (birthday_dt.Date > todays_dt.Date)
+                //  Get todays date
+               // MessageBox.Show("Birthday " + birthday_dt + "\n" +
+                 //               "Today" + todays_dt + "\n" +
+                //                "Before Noon" + beforeNoon + "\n" +
+                //                "Before Five" + beforeFive + "\n" +
+                 //               "After Five" + afterFive);
+
+
+               
+                //  Is it their birthday
+                if (birthday_dt.Date == todays_dt.Date)
                 {
-                    // MessageBox.Show("Todays date " + dt2)
-                    // MessageBox.Show("Todays date " + dt2);
-                    //It's a later date
+                    // Sing Happy Birthday
+                    MessageBox.Show("Happy Birthday to you!\n" +
+                                    "Happy Birthday to you!\n" +
+                                    "Happy Birthday Dear " + name_textbox.Text + "\n" +
+                                    "Happy Birthday to youuuuuuuuuuu!");
                 }
+                
+                // Not birthday, check before noon
+                else if (todays_dt < beforeNoon)
+                {
+                    MessageBox.Show("Good morning " + name_textbox.Text);
+                }
+                
+                //  Cjecl before 5:00
+                else if (todays_dt < beforeFive)
+                {
+                    MessageBox.Show("Time for Happy Hour, margarita and nachos! " + name_textbox.Text);
+                }
+
+                //  Default after 5:00
                 else
                 {
-                    //It's an earlier or equal date
+                    MessageBox.Show("Good night and sleep well! " + name_textbox.Text);
                 }
-            }
-        }
+            } // End IF
+        } // enter_button_Click
 
+        //  Event triggered when selecting a date from the month calendar
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
             your_birthday_textbox.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
         }
 
-        public static DateTime ConvertDate(string in_date, string pattern)
-        {
-            MessageBox.Show("pattern " + pattern);
-            DateTime retval = DateTime.MinValue; //Represents the smallest possible value of DateTime. This field is read-only.
-
-            if (System.DateTime.TryParseExact(in_date, 
-                pattern, 
-                CultureInfo.InvariantCulture,
-                DateTimeStyles.None, 
-                out retval))
-                return retval;
-
-            // Could not convert date..
-            return DateTime.MinValue;
-        }
-
-        
-    }
-}
+       
+    }   //  End prtial class
+}       //  End namespace Happy Birthday
