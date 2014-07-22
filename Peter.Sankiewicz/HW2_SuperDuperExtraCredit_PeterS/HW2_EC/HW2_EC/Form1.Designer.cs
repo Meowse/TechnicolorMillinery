bb95ea95346a94c8ccfb01d2b976e317973189ea
petsan <petsan@hotmail.com>
2014-07-22 14:43:20 -0700

    Updates to HW2

--- Peter.Sankiewicz/HW2_SuperDuperExtraCredit_PeterS/HW2_EC/HW2_EC/Form1.Designer.cs
+++ Peter.Sankiewicz/HW2_SuperDuperExtraCredit_PeterS/HW2_EC/HW2_EC/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.greetingsLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(113, 38);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(100, 20);
+            this.nameField.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Greet Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GreetThem);
+            // 
+            // greetingsLabel
+            // 
+            this.greetingsLabel.AutoSize = true;
+            this.greetingsLabel.Location = new System.Drawing.Point(72, 61);
+            this.greetingsLabel.Name = "greetingsLabel";
+            this.greetingsLabel.Size = new System.Drawing.Size(58, 13);
+            this.greetingsLabel.TabIndex = 2;
+            this.greetingsLabel.Text = "                 ";
+            this.greetingsLabel.Click += new System.EventHandler(this.greetingsLabel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hello, please enter your name and click \"Greet Me!\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(644, 218);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.greetingsLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nameField);
+            this.Name = "Form1";
+            this.Text = "Greet Me!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label greetingsLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Peter.Sankiewicz/HW2_SuperDuperExtraCredit_PeterS/HW2_EC/HW2_EC/Form1.cs
+++ Peter.Sankiewicz/HW2_SuperDuperExtraCredit_PeterS/HW2_EC/HW2_EC/Form1.cs
@@ -15,6 +15,24 @@ namespace HW2_EC
         public Form1()
         {
             InitializeComponent();
+            greetingsLabel.Text = "";
         }
+
+        private void GreetThem(object sender, EventArgs e)
+        {
+            String userName  = nameField.Text;
+            String greeting = "Hello, " + userName;
+            greetingsLabel.Text = greeting;
+        }
+
+        private void greetingsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Options:
+        // 1) Display a message box saying "Please enter your name."
+        // 2) Have it say "Hello" without a name.
+        // 3) Have it say "Hello! What's your name?
+        // 4) Add instructions and then display the error message box.
     }
 }
