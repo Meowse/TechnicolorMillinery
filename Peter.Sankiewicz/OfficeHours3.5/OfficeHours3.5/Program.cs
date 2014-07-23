67ea916f60dcbe2d57dfe7812ca45106a429fe44
petsan <petsan@hotmail.com>
2014-07-23 16:59:06 -0700

    refactor hw 3.5

--- Peter.Sankiewicz/OfficeHours3.5/OfficeHours3.5/Program.cs
+++ Peter.Sankiewicz/OfficeHours3.5/OfficeHours3.5/Program.cs
@@ -12,21 +12,21 @@ namespace OfficeHours3._5
         static void Main(string[] args)
         {
             Console.WriteLine("0 in binary should be: 0, and is: "
-                + GetAsBinary(0));
+                 + GetAsBinary(0));
             Console.WriteLine("1 in binary should be: 1, and is: "
-                + GetAsBinary(1));
+                 + GetAsBinary(1));
             Console.WriteLine("4 in binary should be: 100, and is: "
-                + GetAsBinary(4));
+                 + GetAsBinary(4));
             Console.WriteLine("13 in binary should be: 1101, and is: "
-                + GetAsBinary(13));
+                 + GetAsBinary(13));
             Console.WriteLine("27 in binary should be: 11011, and is: "
-                + GetAsBinary(27));
+                 + GetAsBinary(27));
             Console.WriteLine("31 in binary should be: 11111, and is: "
-                + GetAsBinary(31)); 
+                 + GetAsBinary(31)); 
             Console.WriteLine("32 in binary should be: 100000, and is: "
-                + GetAsBinary(32));
+                 + GetAsBinary(32));
             Console.WriteLine("33 in binary should be: 100001, and is: "
-                + GetAsBinary(33));
+                 + GetAsBinary(33));
             Console.WriteLine("\nPress Any Key to Continue...");
             Console.ReadKey();
         }
@@ -37,41 +37,42 @@ namespace OfficeHours3._5
             {
                 return "0";
             }
-            //1. find the power of two that is smaller or equal to the number
-            //2. subtract that power of 2 from the number, and then (if there is nothing left) 
-            //   find the next smaller power of 2 and subtract it from the remainder, until you get 0
 
-            // is n larger than x
-            int powerOf2 = 1;
-            while (powerOf2 <= decimalValue)
-            {
-                powerOf2 = powerOf2*2; 
-            }
-            powerOf2 = powerOf2/2;
-
-            string binaryValue = "";
+            int largestPowerOf2 = GetLargestPowerOf2(decimalValue);
             
+            return ConvertToBinary(decimalValue, largestPowerOf2);
+        }
+
+        // Converst a decimal value to a binary value (represented as a string),
+        // given the largest power of 2 that the decimal value contains.
+        private static string ConvertToBinary(int decimalValue,int powerOf2)
+        {
+            string binaryValue = "";
+
             while (powerOf2 > 0)
             {
-                //either the remaining decimalValue contains the current power of 2, or it doesn't.
-                //if it does contain the power of 2:
-                //    subtract it from the decimal number
-                //    add a "1" to binaryValue to record that it contained this power of 2
-                //if it doesn't contain the current power of two
-                //    find the next power of two
-                //    add a 0 binaryValue to record that it did not contain this power of 2
                 if (decimalValue >= powerOf2)
                 {
+                    binaryValue = binaryValue + "1"; 
                     decimalValue = decimalValue - powerOf2;
-                    binaryValue = binaryValue + "1";
                 }
                 else
                 {
                     binaryValue = binaryValue + 0;
                 }
-                powerOf2 = powerOf2/2;
+                powerOf2 = powerOf2 / 2;
             }
             return binaryValue;
+        }
+
+        private static int GetLargestPowerOf2(int decimalValue)
+        {
+            int powerOf2 = 1;
+            while (powerOf2 <= decimalValue)
+            {
+                powerOf2 = powerOf2 * 2;
+            }
+            return powerOf2 = powerOf2 / 2;
         }
     }
 }
