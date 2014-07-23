0792f49365f9dc50a24e18389639c7a807a9060a
weijingzhang <zhangiva@gmail.com>
2014-07-22 22:26:05 -0700

    Homework3(part 1)

---
+++ Weijing.Zhang/Homework3/HW2 super duper extra credit/HW2 super duper extra credit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_super_duper_extra_credit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input your score(0-100"); 
+
+            string x = Console .ReadLine (); 
+
+            int  y = int.Parse (x);  
+
+            if (y < 0 || y > 100)
+            { Console.WriteLine("Errer input"); 
+                return; }
+
+            if (y >= 90) Console.WriteLine("Excellent!");
+
+            else if (y >= 80) Console.WriteLine("good");
+
+            else if (y >= 60) Console.WriteLine("Fair");
+
+            else Console.WriteLine("Bad");
+            Console.ReadKey();
+        }
+    }
+}
