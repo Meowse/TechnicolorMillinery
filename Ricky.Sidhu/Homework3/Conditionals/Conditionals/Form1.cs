cc8f2754da42197038a1df12e76d1408e0c16323
ricksi <sidhu_rick20@yahoo.ca>
2014-07-23 20:10:49 -0700

    Homework 3 Conditionals
    Doesn't work but I think I will come back to it :)

--- Ricky.Sidhu/Homework3/Conditionals/Conditionals/Form1.cs
+++ Ricky.Sidhu/Homework3/Conditionals/Conditionals/Form1.cs
@@ -12,9 +12,43 @@ namespace Conditionals
 {
     public partial class Form1 : Form
     {
+        int valueToDisplay;
+        String greeting;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            valueToDisplay = valueToDisplay + 1;
+            greetingsLabel.Text = valueToDisplay.ToString();
+            if (valueToDisplay >= 10)
+            {
+                button1.Enabled = false;
+                MessageBox.Show("Number is too big!!");
+                valueToDisplay = 0;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String userName = nameField.Text;
+            greetingsLabel.Text = greeting;
+            if (userName == "Mickey")
+            {
+                greeting = "Hello, teacher!";
+            }
+            else
+            {
+                greeting = "Hello, " + userName;
+
+            }
+        }
     }
+
 }
+
+ 
