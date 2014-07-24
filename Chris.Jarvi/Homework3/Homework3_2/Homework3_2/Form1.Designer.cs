28a75fa1c3cefaa22019287edc4872807c1f8a90
shatterpaw <mybaldhead@hotmail.com>
2014-07-23 19:50:10 -0700

    Homework 3

---
+++ Chris.Jarvi/Homework3/Homework3_2/Homework3_2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Homework3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.btnGreet = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(212, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter your birthdate";
+            // 
+            // dtBirthdate
+            // 
+            this.dtBirthdate.Location = new System.Drawing.Point(12, 77);
+            this.dtBirthdate.Name = "dtBirthdate";
+            this.dtBirthdate.Size = new System.Drawing.Size(212, 20);
+            this.dtBirthdate.TabIndex = 3;
+            // 
+            // btnGreet
+            // 
+            this.btnGreet.Location = new System.Drawing.Point(149, 103);
+            this.btnGreet.Name = "btnGreet";
+            this.btnGreet.Size = new System.Drawing.Size(75, 23);
+            this.btnGreet.TabIndex = 4;
+            this.btnGreet.Text = "Greet Me!";
+            this.btnGreet.UseVisualStyleBackColor = true;
+            this.btnGreet.Click += new System.EventHandler(this.Greet);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(256, 13);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 168);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnGreet);
+            this.Controls.Add(this.dtBirthdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Homework 3 - #2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtBirthdate;
+        private System.Windows.Forms.Button btnGreet;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Chris.Jarvi/Homework3/Homework3_2/Homework3_2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Greet(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            DateTime birthDate = dtBirthdate.Value;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(String.Format("Hello, {0}", txtName.Text));
+            sb.AppendLine();
+            if(birthDate.Month == today.Month && birthDate.Day == today.Day)
+            {
+                sb.AppendLine("Happy birthday to you");
+                sb.AppendLine("Happy birthday to you");
+                sb.AppendLine(String.Format("Happy birthday, dear {0}", txtName.Text));
+                sb.AppendLine("Happy birthday to you");
+                lblOutput.Text = sb.ToString();
+            }
+            else
+            {
+                if (today.Hour < 12)
+                {
+                    sb.AppendLine("Good morning, good morning, good morning to you");
+                    sb.AppendLine("Good morning, good morning, good morning to you");
+                    sb.AppendLine("Our day is beginning, there's so much to do");
+                    sb.AppendLine("So, good morning, good morning, good morning to you");
+                    lblOutput.Text = sb.ToString();
+                } else if (today.Hour < 17)
+                {
+                    lblOutput.Text = String.Format("Sorry {0}, I'm taking a siesta", txtName.Text);
+                }
+                else
+                {
+                    sb.AppendLine("Rock-a-bye baby, in the treetop");
+                    sb.AppendLine("When the wind blows, the cradle will rock");
+                    sb.AppendLine("When the bough breaks, the cradle will fall");
+                    sb.AppendLine("And down will come baby, cradle and all");
+                    lblOutput.Text = sb.ToString();
+                }
+            }
+        }
+    }
+}
