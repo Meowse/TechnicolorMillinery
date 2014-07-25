00507cbb781a99638a3e17db868ddbfab292dbd2
cdelapena <delapc@gmail.com>
2014-07-24 20:27:27 -0700

    Homework 4 update
    Homework 3 seriously not kidding EC

---
+++ Chris.DelaPena/Homework 4/HW4 Part 3 (HW3 SIAKEC)/HW4 Part 3 (HW3 SIAKEC)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Part_3__HW3_SIAKEC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- Chris.DelaPena/Homework 4/Homework 4 Part 2 (HW3 SDEC)/Homework 4 Part 2 (HW3 SDEC)/Program.cs
+++ Chris.DelaPena/Homework 4/Homework 4 Part 2 (HW3 SDEC)/Homework 4 Part 2 (HW3 SDEC)/Program.cs
@@ -10,6 +10,43 @@ namespace Homework_4_Part_2__HW3_SDEC_
     {
         static void Main(string[] args)
         {
+            //get user's age
+            Console.Write("How old are you?");
+            String stringAge = Console.ReadLine();
+            int age = Int32.Parse(stringAge);
+            //print out user's age in binary using while loop
+
+            // Find the largest power of two for the given age
+            int powerOf2 = 2;
+            while (age / powerOf2 > 0)
+            {
+                powerOf2 = powerOf2 * 2;
+            }
+            powerOf2 = powerOf2 / 2;
+            // MessageBox.Show(powerOf2.ToString());
+
+            // Calculate age in binary
+            int remainderAge = age;
+            string binaryAge = "";
+            while (powerOf2 > 0)
+            {
+                if (remainderAge / powerOf2 > 0)
+                {
+                    binaryAge = binaryAge + "1";
+                    remainderAge = remainderAge - powerOf2;
+                }
+                else
+                {
+                    binaryAge = binaryAge + "0";
+                }
+                powerOf2 = powerOf2 / 2;
+            }
+
+            // MessageBox.Show(binaryAge);
+            Console.WriteLine("Your age in binary is " + binaryAge);
+            Console.ReadKey();
+
+            //Check office hours
         }
     }
 }
