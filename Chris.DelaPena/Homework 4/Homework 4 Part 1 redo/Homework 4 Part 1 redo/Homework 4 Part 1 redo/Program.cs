5db59faeba0946f8289f55f74558435c290d427a
cdelapena <delapc@gmail.com>
2014-07-24 20:03:09 -0700

    Homework 4 update
    Beginning problem 2

--- Chris.DelaPena/Homework 4/Homework 4 Part 1 redo/Homework 4 Part 1 redo/Homework 4 Part 1 redo/Program.cs
+++ Chris.DelaPena/Homework 4/Homework 4 Part 1 redo/Homework 4 Part 1 redo/Homework 4 Part 1 redo/Program.cs
@@ -11,14 +11,23 @@ namespace Homework_4_Part_1_redo
         static void Main(string[] args)
         {
             int songLine = 0;
-            while (songLine < 4)
+            while (songLine < 3)
             {
-                Console.WriteLine("Happy birthday to you !");
+                Console.WriteLine("Happy birthday to you!");
                 if (songLine == 2)
                 {
                     Console.WriteLine("Happy birthday dear user!");
                 }
                 songLine++;
+            }
+            Console.WriteLine("How old are you?");
+            String ageString = Console.ReadLine();
+            int age = int.Parse(ageString);
+            int counter = 1;
+            while (counter < age)
+            {
+                Console.WriteLine("Happy birthday to you!");
+                counter++;
             }
             Console.ReadKey();
         }
---
+++ Chris.DelaPena/Homework 4/Homework 4 Part 2 (HW3 SDEC)/Homework 4 Part 2 (HW3 SDEC)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4_Part_2__HW3_SDEC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
