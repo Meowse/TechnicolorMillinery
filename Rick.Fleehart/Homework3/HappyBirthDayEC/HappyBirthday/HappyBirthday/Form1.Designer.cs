2e598756402b1d1b065c983c8a03d42a5fe6475c
rickfleehart <rickfleehart@gmail.com>
2014-07-26 21:26:03 -0700

    Homework3 EC Final commit
    this tell you how old you are from the entered date and recites Vogon
    poetry in the evening but only enough to get you sick. Or a fun time on
    a Saturday night!

---
+++ Rick.Fleehart/Homework3/HappyBirthDayEC/HappyBirthday/HappyBirthday/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HappyBirthday
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.AskNameLabel = new System.Windows.Forms.Label();
+            this.SelectedDate = new System.Windows.Forms.Label();
+            this.ChooseBDaylabel = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.todaysdate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(53, 20);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(206, 29);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Is it your Birthday?";
+            this.TitleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(190, 76);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 1;
+            // 
+            // AskNameLabel
+            // 
+            this.AskNameLabel.AutoSize = true;
+            this.AskNameLabel.Location = new System.Drawing.Point(50, 79);
+            this.AskNameLabel.Name = "AskNameLabel";
+            this.AskNameLabel.Size = new System.Drawing.Size(134, 13);
+            this.AskNameLabel.TabIndex = 3;
+            this.AskNameLabel.Text = "1. Please enter Your Name";
+            // 
+            // SelectedDate
+            // 
+            this.SelectedDate.AutoSize = true;
+            this.SelectedDate.Location = new System.Drawing.Point(131, 146);
+            this.SelectedDate.Name = "SelectedDate";
+            this.SelectedDate.Size = new System.Drawing.Size(167, 13);
+            this.SelectedDate.TabIndex = 4;
+            this.SelectedDate.Text = "(choose from the calendar please)";
+            // 
+            // ChooseBDaylabel
+            // 
+            this.ChooseBDaylabel.AutoSize = true;
+            this.ChooseBDaylabel.Location = new System.Drawing.Point(50, 112);
+            this.ChooseBDaylabel.Name = "ChooseBDaylabel";
+            this.ChooseBDaylabel.Size = new System.Drawing.Size(271, 13);
+            this.ChooseBDaylabel.TabIndex = 5;
+            this.ChooseBDaylabel.Text = "2. Choose your birthday from the calendar on the right ->";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(418, 29);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 6;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Your Birthday:";
+            // 
+            // todaysdate
+            // 
+            this.todaysdate.AutoSize = true;
+            this.todaysdate.Location = new System.Drawing.Point(470, 4);
+            this.todaysdate.Name = "todaysdate";
+            this.todaysdate.Size = new System.Drawing.Size(62, 13);
+            this.todaysdate.TabIndex = 9;
+            this.todaysdate.Text = "todays date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 371);
+            this.Controls.Add(this.todaysdate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.ChooseBDaylabel);
+            this.Controls.Add(this.SelectedDate);
+            this.Controls.Add(this.AskNameLabel);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.Label AskNameLabel;
+        private System.Windows.Forms.Label SelectedDate;
+        private System.Windows.Forms.Label ChooseBDaylabel;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label todaysdate;
+    }
+}
+
---
+++ Rick.Fleehart/Homework3/HappyBirthDayEC/HappyBirthday/HappyBirthday/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HappyBirthday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // tell them how old they are in years.
+
+        public void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            // Puts the selected date into the label: SelectedDate
+            SelectedDate.Text = monthCalendar1.SelectionRange.Start.ToShortDateString();
+            String theSelectedDate = monthCalendar1.SelectionRange.Start.ToShortDateString();
+
+            // retreive and store the entered name
+            String EnteredName = NametextBox.Text;
+        
+            // find the month, day and year from the enterd date
+            var birthDateString = theSelectedDate.Split(new[] { '/' }, System.StringSplitOptions.RemoveEmptyEntries);
+            String thebirthMonth   = birthDateString[0];
+            String thebirthDay     = birthDateString[1];
+            String thebirthYear    = birthDateString[2];
+
+            //Create todays Date and all of its individual parts
+            DateTime TodayDateTime = DateTime.Now;
+            String TodaysYear = TodayDateTime.Year.ToString();
+            String TodaysMonth = TodayDateTime.Month.ToString();
+            String TodaysDay = TodayDateTime.Day.ToString();
+            String TodaysHour = TodayDateTime.Hour.ToString();
+
+            int Noon = 12;
+            int Evening = 17; //millatary time 5PM is 12 noon plus 5 hours
+
+            //Create song variables and load them with mirth
+            String BirthdaySong = "Happy Birthday to you\nHappy Birthday to you\nHappy Birthday to you\n";
+            BirthdaySong += "Happy Birthday dear " + EnteredName + "\nHappy Birthday to you";
+
+            String MorningSong = "Its early in the morning\nand the birds are just getting up\n";
+            MorningSong += "go get some coffee " + EnteredName + "\n";
+            MorningSong += "and enjoy another day!";
+
+            // you asked for it: Vogon poetry in the evening
+            String SeistaSong = "Oh freddled gruntbuggly, Thy micturitions are to me, \n" ;
+            SeistaSong += "As plurdled gabbleblotchits, On alurgid bee,\n";
+            SeistaSong += "...blah blah blah, Vogon poetry annoys me";
+
+            String EveningSong = "It getting dark " + EnteredName + "\n";
+            EveningSong += "You better get inside before the zombies get you!\n";
+            EveningSong += "and if you are still alive have a nice sleep\n";
+
+
+            if (thebirthMonth == TodaysMonth && thebirthDay == TodaysDay)
+            {
+                // if today is birthday (month & day are today, display Happy birthday song message box
+                MessageBox.Show(BirthdaySong + HowManyYearsOld(thebirthYear, TodaysYear));
+            }
+            else
+            {
+                if (Convert.ToInt32(TodaysHour) < Noon)
+                {
+                    // if today is not birthday but morning display good morning into  message box
+                    MessageBox.Show(MorningSong + HowManyYearsOld(thebirthYear, TodaysYear));
+                }
+
+                if (Convert.ToInt32(TodaysHour) < Evening)
+                {
+                    // if today is not birthday but noon to 5pm display is taking a siesta into message box
+                    MessageBox.Show(SeistaSong + HowManyYearsOld(thebirthYear, TodaysYear));
+                }
+                if (Convert.ToInt32(TodaysHour) > Evening)
+                {
+                    // if today is not birthday but after 5PM then display lullaby into message box
+                    MessageBox.Show(EveningSong + HowManyYearsOld(thebirthYear, TodaysYear));
+                }
+            }
+       }
+
+
+        public String HowManyYearsOld(String BirthYear, String TodaysYear) {
+            // subtract todyas year from the entered year to find out how old they are
+            int YearsOld = Convert.ToInt32(TodaysYear) - Convert.ToInt32(BirthYear);
+
+            return "\nYou are " + YearsOld.ToString() + " years old today!";
+    
+        }
+        //------------------ old code home -----------------------------
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //This should not be here but if I remove it everything explodes
+        }
+    }
+}
