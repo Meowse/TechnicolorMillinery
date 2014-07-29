b1ddfc0515a91f36094d3c71a254831f8fcbbade
cdelapena <delapc@gmail.com>
2014-07-28 21:24:30 -0700

    Homework 4 part 3 complete

--- Chris.DelaPena/Homework 4/HW4 Part 3 (HW3 SIAKEC)/HW4 Part 3 (HW3 SIAKEC)/Program.cs
+++ Chris.DelaPena/Homework 4/HW4 Part 3 (HW3 SIAKEC)/HW4 Part 3 (HW3 SIAKEC)/Program.cs
@@ -6,10 +6,70 @@ using System.Threading.Tasks;
 
 namespace HW4_Part_3__HW3_SIAKEC_
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            happyBirthday();
+            Console.WriteLine("What is your age? ");
+            String input = Console.ReadLine();
+            int age = Convert.ToInt32(input);
+            Console.WriteLine("Your age in binary is: " + GetAsBinary(age));
+            Console.ReadKey();
+        }
+
+        private static void happyBirthday()
+        {
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear user!");
+            Console.WriteLine("Happy birthday to you!!");
+        }
+
+        private static string GetAsBinary(int decimalValue)
+        {
+            if (decimalValue == 0)
+            {
+                return "0";
+            }
+            int largestPowerOf2 = GetLargestPowerOf2(decimalValue);
+
+            return ConvertToBinary(decimalValue, largestPowerOf2);
+        }
+
+
+        private static int GetLargestPowerOf2(int decimalValue)
+        {
+            int powerOf2 = 1;
+            while (powerOf2 <= decimalValue)
+            {
+                powerOf2 = powerOf2*2;
+            }
+            return powerOf2/2;
+        }
+
+        // Convert a decimal value to a binary value (represented as a 
+        // string), given the largest power of 2 that the decimal value
+        // contains.
+        private static string ConvertToBinary(int decimalValue, int powerOf2)
+        {
+            string binaryValue = "";
+
+            while (powerOf2 > 0)
+            {
+                if (decimalValue >= powerOf2)
+                {
+                    binaryValue = binaryValue + "1";
+                    decimalValue = decimalValue - powerOf2;
+                }
+                else
+                {
+                    binaryValue = binaryValue + "0";
+                }
+                powerOf2 = powerOf2/2;
+            }
+
+            return binaryValue;
         }
     }
 }
