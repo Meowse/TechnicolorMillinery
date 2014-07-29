654e43c6648f044ccbd6e638f562012261350692
dcgutman <chrisgutman@gmail.com>
2014-07-28 21:39:52 -0500

    HW 3, Happy Birthday Base Application, extra credit to follow

---
+++ Chris.Gutman/Homework3/Question 2 of HW3/Happy Birthday/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BirthdayBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(26, 55);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(232, 22);
+            this.NameBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Name";
+            // 
+            // BirthdayBox
+            // 
+            this.BirthdayBox.Location = new System.Drawing.Point(26, 128);
+            this.BirthdayBox.Name = "BirthdayBox";
+            this.BirthdayBox.Size = new System.Drawing.Size(232, 22);
+            this.BirthdayBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter your birthday";
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(29, 197);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(75, 23);
+            this.GoButton.TabIndex = 2;
+            this.GoButton.Text = "Go";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.Calculate);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(182, 196);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.Bye);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 253);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BirthdayBox);
+            this.Controls.Add(this.NameBox);
+            this.Name = "Form1";
+            this.Text = "Your Birthday";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Calculate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BirthdayBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Chris.Gutman/Homework3/Question 2 of HW3/Happy Birthday/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private DateTime NoonToday = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 12, 0, 0);
+        private DateTime FivepmToday = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 17, 0, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bye(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+
+/* Use visual studios namespace for regular expressions this time instead of attempting to
+   add a reference to VB script and use that convoluted set up used on last homework problem.... */
+
+            string mystring = @"(((\s+)?\d{0,2}(\s+)?[/\-]){2}(\s+)?(\d\d)?\d\d)" +
+                @"|((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|nov|Dec)(\s+)(\d+),?(\s+)(\d\d)?\d\d)";
+            
+            Regex re = new Regex(mystring);
+            RegexOptions reoptions = new RegexOptions();
+            reoptions = RegexOptions.Singleline | RegexOptions.IgnoreCase;
+            Match rematch = re.Match(BirthdayBox.Text);
+            
+/* this should place a validated date in mystring. If it is populated, use the DateTime facility to implement the rest of the 
+      comparisons asked for in the homework*/
+
+            mystring = rematch.Value.ToString();    
+
+/* If there was a valid birth day found ... */
+
+            if(mystring != "") 
+            {
+                DateTime birthdate = DateTime.Parse(mystring);
+                DateTime birthday = new DateTime(DateTime.Today.Year, birthdate.Month, birthdate.Day);
+                DateTime DateToday = DateTime.Today;
+                int answer = DateTime.Compare(birthday, DateToday);
+                
+/*     If today is the user's birthday, answer will == 0     */
+                
+                if (answer == 0)
+                {
+                    MessageBox.Show("Happy Birthday to you" +
+                                        "\nHappy Birthday to you" +
+                                        "\nHappy Birthday dear" + NameBox.Text +
+                                        "\nHappy Birthday to you!!");
+                }
+                else if (DateTime.Compare(DateTime.Now, NoonToday)<0)
+                {
+                    MessageBox.Show("Good Morning to you, good morning to you....");
+
+                }
+                else if (DateTime.Compare(DateTime.Now, FivepmToday) > 0)
+                {
+                    MessageBox.Show("Lullaby, lullaby, and good night ... ");
+                }
+                else if ((DateTime.Compare(DateTime.Now, FivepmToday) <= 0) & (DateTime.Compare(DateTime.Now, NoonToday) >= 0)) 
+                {
+                    MessageBox.Show("Nice of you to join us today.  Unfortunately, we are taking a siesta...");
+                }
+
+
+            }
+
+/*    Here, we were unable to interpret the user's entry for their birthday, ask them to re-enter... */
+
+            else
+            {
+                MessageBox.Show(NameBox.Text + ", please enter your birthdate on earth");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
