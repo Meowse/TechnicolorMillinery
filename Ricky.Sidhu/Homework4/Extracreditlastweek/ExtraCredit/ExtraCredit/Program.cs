931b2f54ca4193d68cba6c039b544e6edeebeb11
ricksi <sidhu_rick20@yahoo.ca>
2014-07-28 18:14:45 -0700

    Extra Credit for Homework 3

--- Ricky.Sidhu/Homework4/Extracreditlastweek/ExtraCredit/ExtraCredit/Program.cs
+++ Ricky.Sidhu/Homework4/Extracreditlastweek/ExtraCredit/ExtraCredit/Program.cs
@@ -11,16 +11,26 @@ namespace ExtraCredit
         static void Main(string[] args)
         {
             int rhymeLine = 0;
-            while (rhymeLine <6)
+            while (rhymeLine <5)
             {
                 Console.WriteLine("Happy Birthday to you! You might live in a zoo!!");
-                if (rhymeLine ==4)
+                if (rhymeLine ==3)
                 {
                     Console.WriteLine("Happy Birthday dear friend!");
 
                 }
                 rhymeLine++;
            }
+            Console.WriteLine("What is yoru age?");
+            string ageString = Console.ReadLine();
+            int age = int.Parse(ageString);
+            int count = 1;
+            while (count <age)
+            {
+                Console.WriteLine("Happy Birthday to you!!!!");
+                count++;
+            }
+
             Console.ReadKey();
         }
     }
