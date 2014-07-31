4bd0dbcbddeaa63ed54abdaa54284dc956030150
lammersb <bj_lammers@msn.com>
2014-07-30 18:38:51 -0700

    Homework 5 Math Helper creating Test Project and Test Class
    Homework 5 Math Helper creating Test Project and Test Class

---
+++ Bridget.Lammers/Homework5/MathHelper/MathHelper/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers\n");
+            int number1;
+            int number2;
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+
+            MathsHelper helper = new MathsHelper();
+            int x = helper.Add(number1, number2);
+            Console.WriteLine("\nThe sum of " + number1 +
+                " and " + number2 + " is " + x);
+            Console.ReadKey();
+            int y = helper.Subtract(number1, number2);
+            Console.WriteLine("\nThe difference between " +
+                  number1 + " and" + number2 + "  is " + y);
+            Console.ReadKey();
+        }
+    }
+
+    public class MathsHelper
+    {
+        public MathsHelper() { }
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
+
---
+++ Bridget.Lammers/Homework5/MathHelper/MathHelperTests/MyClassTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelperTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+        [TestCase]
+        public void SubtractTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
