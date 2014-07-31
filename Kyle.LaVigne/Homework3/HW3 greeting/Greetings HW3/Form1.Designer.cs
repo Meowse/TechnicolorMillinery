3ed3b742c70d8f64c87e411ec3bb81a3748efe23
kdlavigne <kdlv@uw.edu>
2014-07-30 22:20:52 -0700

    HW3 Edits

--- Kyle.LaVigne/Homework3/HW3 greeting/Greetings HW3/Form1.Designer.cs
+++ Kyle.LaVigne/Homework3/HW3 greeting/Greetings HW3/Form1.Designer.cs
@@ -36,6 +36,16 @@
             this.label3 = new System.Windows.Forms.Label();
             this.BirthdayText = new System.Windows.Forms.TextBox();
             this.BirthdayButton = new System.Windows.Forms.Button();
+            this.GetTime = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BirthMonthText = new System.Windows.Forms.TextBox();
+            this.BirthYearText = new System.Windows.Forms.TextBox();
+            this.BinaryAge = new System.Windows.Forms.Button();
+            this.BinaryLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // GreetingButton
@@ -93,9 +103,9 @@
             // 
             // BirthdayText
             // 
-            this.BirthdayText.Location = new System.Drawing.Point(150, 120);
+            this.BirthdayText.Location = new System.Drawing.Point(133, 123);
             this.BirthdayText.Name = "BirthdayText";
-            this.BirthdayText.Size = new System.Drawing.Size(100, 20);
+            this.BirthdayText.Size = new System.Drawing.Size(34, 20);
             this.BirthdayText.TabIndex = 6;
             // 
             // BirthdayButton
@@ -106,13 +116,111 @@
             this.BirthdayButton.TabIndex = 7;
             this.BirthdayButton.Text = "Done!";
             this.BirthdayButton.UseVisualStyleBackColor = true;
-            this.BirthdayButton.Click += new System.EventHandler(this.BdayButton_Click);
+            this.BirthdayButton.Click += new System.EventHandler(this.BirthdayButton_Click);
+            // 
+            // GetTime
+            // 
+            this.GetTime.Location = new System.Drawing.Point(428, 10);
+            this.GetTime.Name = "GetTime";
+            this.GetTime.Size = new System.Drawing.Size(121, 23);
+            this.GetTime.TabIndex = 8;
+            this.GetTime.Text = "What day is it?";
+            this.GetTime.UseVisualStyleBackColor = true;
+            this.GetTime.Click += new System.EventHandler(this.DateTime_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(477, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Day";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(426, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Month";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(499, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Time";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(520, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Year";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(426, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Time of Day:";
+            // 
+            // BirthMonthText
+            // 
+            this.BirthMonthText.Location = new System.Drawing.Point(173, 123);
+            this.BirthMonthText.Name = "BirthMonthText";
+            this.BirthMonthText.Size = new System.Drawing.Size(34, 20);
+            this.BirthMonthText.TabIndex = 14;
+            // 
+            // BirthYearText
+            // 
+            this.BirthYearText.Location = new System.Drawing.Point(213, 123);
+            this.BirthYearText.Name = "BirthYearText";
+            this.BirthYearText.Size = new System.Drawing.Size(34, 20);
+            this.BirthYearText.TabIndex = 15;
+            // 
+            // BinaryAge
+            // 
+            this.BinaryAge.Location = new System.Drawing.Point(133, 207);
+            this.BinaryAge.Name = "BinaryAge";
+            this.BinaryAge.Size = new System.Drawing.Size(160, 23);
+            this.BinaryAge.TabIndex = 16;
+            this.BinaryAge.Text = "Give me my age in binary";
+            this.BinaryAge.UseVisualStyleBackColor = true;
+            this.BinaryAge.Click += new System.EventHandler(this.BinaryAge_Click);
+            // 
+            // BinaryLabel
+            // 
+            this.BinaryLabel.AutoSize = true;
+            this.BinaryLabel.Location = new System.Drawing.Point(312, 216);
+            this.BinaryLabel.Name = "BinaryLabel";
+            this.BinaryLabel.Size = new System.Drawing.Size(88, 13);
+            this.BinaryLabel.TabIndex = 17;
+            this.BinaryLabel.Text = "                           ";
             // 
             // IntroductionBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 400);
+            this.ClientSize = new System.Drawing.Size(577, 400);
+            this.Controls.Add(this.BinaryLabel);
+            this.Controls.Add(this.BinaryAge);
+            this.Controls.Add(this.BirthYearText);
+            this.Controls.Add(this.BirthMonthText);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GetTime);
             this.Controls.Add(this.BirthdayButton);
             this.Controls.Add(this.BirthdayText);
             this.Controls.Add(this.label3);
@@ -138,6 +246,16 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox BirthdayText;
         private System.Windows.Forms.Button BirthdayButton;
+        private System.Windows.Forms.Button GetTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox BirthMonthText;
+        private System.Windows.Forms.TextBox BirthYearText;
+        private System.Windows.Forms.Button BinaryAge;
+        private System.Windows.Forms.Label BinaryLabel;
     }
 }
 
--- Kyle.LaVigne/Homework3/HW3 greeting/Greetings HW3/Form1.cs
+++ Kyle.LaVigne/Homework3/HW3 greeting/Greetings HW3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,6 @@ namespace Greetings_HW3
 {
     public partial class IntroductionBox : Form
     {
-        String now;
-        String UserName;
         public IntroductionBox()
         {
             InitializeComponent();
@@ -23,7 +22,7 @@ namespace Greetings_HW3
         {
             {
                 String UserName = NameField.Text;
-        
+
                 if (UserName.Length == 0)
                 {
                     MessageBox.Show("Oops, please enter your name above.");
@@ -44,27 +43,137 @@ namespace Greetings_HW3
 
         }
 
-        private void BdayButton_Click(object sender, EventArgs e)
+
+        private void DateTime_Click(object sender, EventArgs e)
         {
-            String BirthDate = BirthdayText.Text;
-            String Username = GreetingLabel.Text;
-            now = DateTime.Today.ToString("MM/DD");
-        
-        if (BirthDate.Length == 0)
-        {
-            MessageBox.Show("Please enter your birthdate in the form MM/DD/YYYY");
+            DateTime theDate;
+
+            theDate = DateTime.Now;
+
+            int theDay = theDate.Day;
+            int theMonth = theDate.Month;
+            int theYear = theDate.Year;
+            int theTime = theDate.Hour;
+            label4.Text = theDay.ToString();
+            label5.Text = theMonth.ToString();
+            label6.Text = theTime.ToString();
+            label7.Text = theYear.ToString();
         }
-            if (BirthDate == now)
+
+        private void BirthdayButton_Click(object sender, EventArgs e)
+        {
+
+            String BirthDay = BirthdayText.Text;
+            String BirthMonth = BirthMonthText.Text;
+            String Birthyear = BirthYearText.Text;
+            String BirthDate = BirthMonthText.Text + BirthdayText.Text;
+            String Username = NameField.Text;
+            DateTime theDate;
+            theDate = DateTime.Now;
+
+            int theDay = theDate.Day;
+            int theMonth = theDate.Month;
+            int theYear = theDate.Year;
+            int theTime = theDate.Hour;
+            label4.Text = theDay.ToString();
+            label5.Text = theMonth.ToString();
+            label6.Text = theTime.ToString();
+            label7.Text = theYear.ToString();
+
+
+            if (BirthdayText.Text == theDay.ToString() && BirthMonthText.Text == theMonth.ToString())
             {
-                MessageBox.Show("Happy birthday to you/n" 
-                   + "Happy birthday to you/n"
-                   + "Happy birthday dear" + UserName + "!"
-                , "Happy Birthday!");
+                Singhappybirthday();
             }
+            else if (theTime < 12)
+            {
+                MessageBox.Show("Good morning," + Username + "!", "Rise and Shine");
+            }
+            else if (theTime < 17)
+            {
+                MessageBox.Show("I am terribly sorry. I'm currently taking a siesta. Come back later!", "Mmmm");
+            }
+            if (theTime > 17)
+            {
+                MessageBox.Show("Lullaby and good night,\n"
+                           + "With roses bedight,\n"
+                           + "With lilies o'er spread\n"
+                           + "Is baby's wee bed.\n"
+                           + "Lay thee down now and rest,\n"
+                           + "May thy slumber be blessed.\n"
         
-    
-    }
-       
+                            + "Lullaby and good night,\n"
+                            + "Thy mother's delight,\n"
+                            + "Bright angels beside\n"
+                            + "My darling abide.\n"
+                            + "They will guard thee at rest,\n"
+                            + "Thou shalt wake on my breast.\n" 
+                            + "\n", "Brahm's Lullaby for " + Username);
+            }
+        }
+
+        private void BinaryAge_Click(object sender, EventArgs e)
+        {
+            DateTime theDate;
+            theDate = DateTime.Now;
+            String Birthyear = BirthYearText.Text;
+            int age = theDate.Year - Convert.ToInt32(BirthYearText.Text);
+            BinaryLabel.Text = GetAsBinary(age);
 
         }
-    }
+
+        private void Singhappybirthday()
+        {
+            DateTime theDate;
+            theDate = DateTime.Now;
+            String Birthyear = BirthYearText.Text;
+            String Username = NameField.Text;
+            int age = theDate.Year - Convert.ToInt32(BirthYearText.Text);
+            if (age == 0)
+            {
+                int i = 0;
+                while (i < age)
+                {
+                    MessageBox.Show("Happy birthday to you\n");
+                    i++;
+
+                }
+            }
+
+
+        }
+
+        private static string GetAsBinary(int age)
+        {
+             if (age == 0)
+          {
+                return "O";
+                    }
+
+                    int PowerOf2 = 1;
+                    while (PowerOf2 <= age)
+                    {
+                        PowerOf2 = PowerOf2*2;
+                    }
+                    PowerOf2 = PowerOf2/2;
+                    string BinaryValue = "";
+                    while (PowerOf2 > 0)
+                    {
+                        if (age >= PowerOf2)
+                        {
+                            age = age - PowerOf2;
+                            BinaryValue = BinaryValue + "1";
+                        }
+                        else
+                        {
+                            BinaryValue = BinaryValue + "0";
+                        }
+                        PowerOf2 = PowerOf2/2;
+                    }
+
+                    return BinaryValue;
+
+                }
+            }
+        }
+    
