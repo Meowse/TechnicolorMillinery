6a483957ddd1ba08d6e34c357ba2254c09154477
lammersb <bj_lammers@msn.com>
2014-07-31 20:06:31 -0700

    Homework 5 Sorter in class

---
+++ Bridget.Lammers/Homework5/Sorter/SortThings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortUtility sorter = new SortUtility(); 
+
+            Console.WriteLine("Enter numbers to sort, or 'exit' to exit.");
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                int[] values = ParseInput(input);
+                sorter.Sort(values);
+                DisplayOut(values);
+            }
+
+        }
+    }
+}
---
+++ Bridget.Lammers/Homework5/Sorter/Sorter/SortUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    public class SortUtility
+    {
+        public void Sort(int[] values)
+        {
+           // Read a value with "values[3]"
+            // Write a value with "values[2] = 17"
+            // Get the number of values with "values.length"
+
+            // First value in the array is "values[0]"
+            //  Last value in the array is "values[values.Length - 1]"
+
+            int[] newArray = new int[7];
+            for (int i = 0; i < values.Length; i++)
+            {
+                newArray[i] = values[i];
+
+            }
+
+        }
+    }
+}
