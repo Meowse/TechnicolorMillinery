6c82fb92488d680c2bb95f29cd952020724a164a
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-08-01 14:51:46 -0700

    Sort Lab ~ corrected works now

--- Cathy.Blanscet/Session6Lab/Sorter/Sorter/SortUtility.cs
+++ Cathy.Blanscet/Session6Lab/Sorter/Sorter/SortUtility.cs
@@ -6,7 +6,6 @@ namespace Sorter
     {
         public void Sort(int[] values)
         {
-
             // Read a value with "values[3]"
             // Write a value with "values[2] = 17"
             // Get the number of values with "values.Length"
@@ -18,14 +17,17 @@ namespace Sorter
             for (int i = 0; i < values.Length; i++)
             {
                 //values[i] = values[i] * 2;
-                if (values[i] > values[i + 1]) //unhandled out of bounds error here
+                for (int ndx = i+1; ndx < values.Length; ndx++)
                 {
-                    int tempInt = values[i];
-                    values[i] = values[i + 1];
-                    values[i + 1] = tempInt;
+                    if (values[i] > values[ndx])
+                    {
+                        int tempInt = values[i];
+                        values[i] = values[ndx];
+                        values[ndx] = tempInt;
+                    }
                 }
             }
-
         }
     }
 }
+
