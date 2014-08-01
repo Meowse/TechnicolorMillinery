a172e70808e4d677d9625b6ee08ac328a3b7cb38
dcgutman <chrisgutman@gmail.com>
2014-07-31 22:30:47 -0500

    Simple Sort Check in

--- Chris.Gutman/Sorter/Sorter/SortUtility.cs
+++ Chris.Gutman/Sorter/Sorter/SortUtility.cs
@@ -4,6 +4,8 @@
     {
         public void Sort(int[] values)
         {
+            bool sortagain = new bool();
+            int tempvar = new int();
             // Read a value with "values[3]"
             // Write a value with "values[2] = 17"
             // Get the number of values with "values.Length"
@@ -12,9 +14,22 @@
             // Last value in the array is "values[values.Length - 1]"
 
             // Example of using a for-loop to double all elements of an array
-            for (int i = 0; i < values.Length; i++)
+            sortagain = true;
+            while (sortagain)
             {
-                values[i] = values[i] * 2;
+                sortagain = false;
+                for (int i = 0; i < values.Length - 1; i++)
+                {
+                    if (values[i] > values[i + 1])
+                    {
+                        tempvar = values[i + 1];
+                        values[i + 1] = values[i];
+                        values[i] = tempvar;
+                        sortagain = true;
+                    }
+
+                }
+                
             }
         }
     }
