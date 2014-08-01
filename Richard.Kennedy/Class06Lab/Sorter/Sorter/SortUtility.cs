3757cee23ec98d432d6c2d2c55b232fd42d46293
RichKenn <RichKenn@myuw.net>
2014-07-31 20:42:12 -0700

    Class 06 Lab Final

--- Richard.Kennedy/Class06Lab/Sorter/Sorter/SortUtility.cs
+++ Richard.Kennedy/Class06Lab/Sorter/Sorter/SortUtility.cs
@@ -14,7 +14,15 @@
             // Example of using a for-loop to double all elements of an array
             for (int i = 0; i < values.Length; i++)
             {
-                values[i] = values[i] * 2;
+                for (int j = (i + 1); j < values.Length; j++)
+                {
+                    if (values[i] > values[j])
+                    {
+                        int temp = values[i];
+                        values[i] = values[j];
+                        values[j] = temp;
+                    }
+                }
             }
         }
     }
