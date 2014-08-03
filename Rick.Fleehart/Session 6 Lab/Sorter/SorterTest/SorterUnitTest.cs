99841b5c1427cef5173072ac1d3cd233b4f0d635
rickfleehart <rickfleehart@gmail.com>
2014-08-02 20:31:44 -0700

    Session 6 Lab Tests initial commit

---
+++ Rick.Fleehart/Session 6 Lab/Sorter/SorterTest/SorterUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Sorter;
+
+namespace SorterTest
+{
+
+    // below copied from Mickey then modified.
+        [TestFixture]
+        public class SorterUnitTests
+        {
+            private SortUtility Sorter = new SortUtility();
+
+            [Test]
+            public void CanSortSingleElementList()
+            {
+                int[] values = {3};
+                int[] sorted = {3};
+                Sorter.Sort(values);
+                Assert.AreEqual(sorted, values);
+            }
+
+            [Test]
+            public void CanSortDoubleElementList()
+            {
+                int[] values = {4, 3};
+                int[] sorted = {3, 4};
+                Sorter.Sort(values);
+                Assert.AreEqual(sorted, values);
+            }
+
+            [Test]
+            public void CanSortTripleElementList()
+            {
+                int[] values = {4, 3, 2};
+                int[] sorted = {2, 3, 4};
+                Sorter.Sort(values);
+                Assert.AreEqual(sorted, values);
+            }
+
+            [Test]
+            // don't think this is reaaly needed since we can do three above
+            public void CanSortQuadElementList()
+            {
+                int[] values = {4, 3, 2};
+                int[] sorted = {2, 3, 4};
+                Sorter.Sort(values);
+                Assert.AreEqual(sorted, values);
+            }
+        }
+    }
+
