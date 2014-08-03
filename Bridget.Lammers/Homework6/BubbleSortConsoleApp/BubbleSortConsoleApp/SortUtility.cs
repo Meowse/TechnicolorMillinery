baf6482901521e6f4ffbb4b1beaa487fa2f5b346
lammersb <bj_lammers@msn.com>
2014-08-03 10:47:50 -0700

    Homework 6 Bubble Sort Algorithm

---
+++ Bridget.Lammers/Homework6/BubbleSortConsoleApp/BubbleSortConsoleApp/SortUtility.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortConsoleApp
+{
+    class SortUtility
+    {
+
+        static int BubbleSorting()
+
+        {
+
+            Console.Write("\nProgram for sorting using Bubble Sort Algorithm");
+
+            Console.Write("\n\nEnter the total number of elements: ");
+
+            int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int[] numarr = new int[max];
+
+
+
+            for (int i = 0; i < max; i++)
+            {
+
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+
+                numarr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+
+            Console.Write("Input int array   : ");
+
+            for (int k = 0; k < max; k++)
+
+                Console.Write(numarr[k] + " ");
+
+            Console.Write("\n");
+
+
+
+            for (int i = 1; i < max; i++)
+            {
+
+                for (int j = 0; j < max - i; j++)
+                {
+
+                    if (numarr[j] > numarr[j + 1])
+                    {
+
+                        int temp = numarr[j];
+
+                        numarr[j] = numarr[j + 1];
+
+                        numarr[j + 1] = temp;
+
+                    }
+
+                }
+
+                Console.Write("Iteration " + i.ToString() + ": ");
+
+                for (int k = 0; k < max; k++)
+
+                    Console.Write(numarr[k] + " ");
+
+                Console.Write("\n");
+
+
+
+            }
+
+
+
+            Console.Write("\n\nThe numbers in ascending orders are given below:\n\n");
+
+            for (int i = 0; i < max; i++)
+            {
+
+                Console.Write("Sorted [" + (i + 1).ToString() + "] element: ");
+
+                Console.Write(numarr[i]);
+
+                Console.Write("\n");
+
+            }
+
+            return 0;
+        }
+
+         static void Main(string[] args)
+
+        {
+
+            BubbleSorting();
+
+            Console.ReadLine();
+        }  // End Main start point
+
+    } // End class
+}   //  End Namespace
---
+++ Bridget.Lammers/Homework6/Sorter/Sorter/SortUtility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Sorter
+{
+    public class SortUtility
+    {
+        public void Sort(int[] values)
+        {
+            // Read a value with "values[3]"
+            // Write a value with "values[2] = 17"
+            // Get the number of values with "values.Length"
+
+            // First value in the array is "values[0]"
+            // Last value in the array is "values[values.Length - 1]"
+
+            // Example of using a for-loop to double all elements of an array
+            //for (int i = 0; i < values.Length; i++)
+            //{
+             //  values[i] = values[i] * 2;
+            //}
+
+            if (values.Length > 1)
+            {
+                if (values[0] > values[1])
+                {
+                    int temp = values[0];
+                    values[0] = values[1];
+                    values[1] = temp;
+                }
+
+            }
+
+            // Simple sort call
+           // i0nt[] values = { 4, 7, 2, 0 };
+           // Array.Sort(values);
+//            foreach (int value in values)
+//            {
+//                Console.Write(value);
+//                Console.Write(' ');
+//            }
+//            Console.WriteLine();
+
+
+           
+        }
+    }
+}
