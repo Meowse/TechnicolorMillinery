5b7f16fc56094900412d0f95e4d19b5b8e2db274
Meowse <meowse@remove.gmail.remove.com>
2014-08-03 17:12:44 -0700

    inlining example baseline

---
+++ Mickey.Phoenix/OfficeHours6.5/InliningExample/InliningExample/InliningExampleClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InliningExample
+{
+    public class InliningExampleClass
+    {
+        public void SomeFunction(string someArg)
+        {
+            int total = 0;
+            int length = someArg.Length;
+            for (int i = 0; i < someArg.Length; i++)
+            {
+                char nextChar = someArg[i];
+                if (char.IsDigit(nextChar))
+                {
+                    int value = int.Parse(nextChar.ToString()) * i;
+                    total = total + value;
+                }
+            }
+            Console.WriteLine("Total for {0} is {1}", someArg, total);
+        }
+    }
+}
---
+++ Mickey.Phoenix/OfficeHours6.5/InliningExample/Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InliningExample;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inliningExample = new InliningExampleClass();
+
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                inliningExample.SomeFunction(input);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
