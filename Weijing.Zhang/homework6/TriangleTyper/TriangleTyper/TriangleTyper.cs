ee2f4fd9c213ef1f2d8df3013671bff9a09dbcaa
weijingzhang <zhangiva@gmail.com>
2014-08-04 23:17:55 -0700

    Homework 6 part 2

---
+++ Weijing.Zhang/homework6/TriangleTyper/TriangleTyper/TriangleTyper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTyper
+{
+    public class TriangleTyper
+    {
+        public string GetType(long a, long b, long c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Weijing.Zhang/homework6/TriangleTyper/TriangleTyperTest/TriangleTyperTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+namespace TriangleTyperTest
+{
+    [TestFixture]
+    public class TriangleTyperTest
+    {
+        TriangleTyper.TriangleTyper triangleTyper = new TriangleTyper.TriangleTyper();
+
+        [Test]
+        public void EquilateralTriangle()
+        {
+            long a = 3;
+            long b = 3;
+            long c = 3;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "Equilateral";
+            Assert.AreEqual(expected, type);
+        }
+
+        [Test]
+        public void InvalidTriangleNegative()
+        {
+            long a = -1;
+            long b = -1;
+            long c = -1;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "NotATriangle";
+            Assert.AreEqual(expected, type);
+        }
+
+        [Test]
+        public void InvalidTriangleZero()
+        {
+            long a = 0;
+            long b = 0;
+            long c = 0;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "NotATriangle";
+            Assert.AreEqual(expected, type);
+        }
+
+        [Test]
+        public void InvalidTriangleOutOfRange()
+        {
+            long a = 1;
+            long b = 2;
+            long c = 3;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "NotATriangle";
+            Assert.AreEqual(expected, type);
+        }
+
+        [Test]
+        public void IsoscelesTriangle()
+        {
+            long a = 2;
+            long b = 2;
+            long c = 3;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "Isosceles";
+            Assert.AreEqual(expected, type);
+        }
+
+        [Test]
+        public void ScaleneTriangle()
+        {
+            long a = 2;
+            long b = 3;
+            long c = 4;
+            string type = triangleTyper.GetType(a, b, c);
+            string expected = "Scalene";
+            Assert.AreEqual(expected, type);
+        }
+    }
+}
