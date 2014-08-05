8f4c332cc4195f9f80a4e8b067f18f8ccbf507f8
cgsusername <sgc321@hotmail.com>
2014-08-04 19:46:43 -0700

    HW_6_Sorter_Sopcak
    Initial attempt sorter

---
+++ Chris.Sopcak/Homework1/ClassLibrary1_Sorter_Sopcak/Sort_Things_Sopcak/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Things_Sopcak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.WriteLine("Enter numbers to sort, or 'exit' to exit.");
+        }
+    }
+}
