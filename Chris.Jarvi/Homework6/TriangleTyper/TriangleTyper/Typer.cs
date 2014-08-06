d6e71bdfa48e3be7410387eb7066e01de625aafe
Chris Jarvi <mybaldhead@hotmail.com>
2014-08-05 18:40:47 -0700

    TriangleTyper

---
+++ Chris.Jarvi/Homework6/TriangleTyper/TriangleTyper/Typer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTyper
+{
+    public class Typer
+    {
+        public string GetType(long side1, long side2, long side3)
+        {
+            if (side1 < 1 || side2 < 1 || side3 < 1)
+            {
+                return "";
+            }
+
+            int equalSides = 0;
+            if (side1 == side2)
+            {
+                equalSides++;
+            }
+            if (side2 == side3)
+            {
+                equalSides++;
+            }
+            if (side1 == side3)
+            {
+                equalSides++;
+            }
+            switch (equalSides)
+            {
+                case 0:
+                    return "Scalene";
+                case 1:
+                    return "Isosceles";
+                case 3:
+                    return "Equilateral";
+                default:
+                    return "";
+            }
+            
+        }
+    }
+}
---
+++ Chris.Jarvi/Homework6/TriangleTyper/TriangleTyperTests/Class1.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using TriangleTyper;
+
+namespace TriangleTyperTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        private TriangleTyper.Typer _typer = new TriangleTyper.Typer();
+
+        [Test]
+        public void CanTypeIsoscelesTriangles()
+        {
+            string expected = "Isosceles";
+            string triangleType = _typer.GetType(3,3,2);
+            Assert.AreEqual(expected, triangleType);
+        }
+
+        [Test]
+        public void CanTypeEquilateralTriangles()
+        {
+            string expected = "Equilateral";
+            string triangleType = _typer.GetType(3,3,3);
+            Assert.AreEqual(expected, triangleType);
+        }
+
+        [Test]
+        public void CanTypeScaleneTriangles()
+        {
+            
+            string expected = "Scalene";
+            string triangleType = _typer.GetType(3,2,1);
+            Assert.AreEqual(expected, triangleType);
+        }
+
+        [Test]
+        public void CanNotTypeNegativeSides()
+        {
+            string expected = "";
+            string triangleType = _typer.GetType(-1, 2, 2);
+            Assert.AreEqual(expected, triangleType);
+        }
+
+        [Test]
+        public void CanNotTypeZeroLengthSide()
+        {
+            string expected = "";
+            string triangleType = _typer.GetType(0, 0, 0);
+            Assert.AreEqual(expected, triangleType  );
+        }
+
+    }
+}
