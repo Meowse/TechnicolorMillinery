832d3ab5653300014d54e36d8ce75dc214b2f4a5
dcgutman <chrisgutman@gmail.com>
2014-08-06 21:26:07 -0500

    Single Unit Test For Sorter Algorithm

---
+++ Chris.Gutman/Sorter/SorterTests/Sortertestclass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sorter;
+
+namespace SorterTests
+{
+    
+    [TestFixture]
+    public class Sortertestclass
+    {
+        SortUtility sorter = new SortUtility();
+        [Test]
+        public void Shouldsort8()
+        {
+            int[] values = {3, 2, 1, 0};
+            int[] actual = {0, 1, 2, 3};
+            sorter.Sort(values);
+            Assert.AreEqual(actual,  values);
+        }
+    }
+}
