c014572dcafa8680b4824f955cbd959e733a4359
kdlavigne <kdlv@uw.edu>
2014-08-06 21:53:18 -0700

    Homework 6

--- Kyle.LaVigne/Session 06/Sorter/Sorter/SortUtility.cs
+++ Kyle.LaVigne/Session 06/Sorter/Sorter/SortUtility.cs
@@ -4,18 +4,23 @@
     {
         public void Sort(int[] values)
         {
-            // Read a value with "values[3]"
-            // Write a value with "values[2] = 17"
-            // Get the number of values with "values.Length"
-
-            // First value in the array is "values[0]"
-            // Last value in the array is "values[values.Length - 1]"
-
-            // Example of using a for-loop to double all elements of an array
+            // Really basic and buggy
             for (int i = 0; i < values.Length; i++)
             {
-                values[i] = values[i] * 2;
+                if (values.Length > 1)
+                {
+                    if (values[0] > values[1])
+                    {
+                        int swapper = values[0];
+                        values[0] = values[1];
+                        values[1] = swapper;
+                    }
+
+                }
             }
+
+
+
         }
     }
 }
