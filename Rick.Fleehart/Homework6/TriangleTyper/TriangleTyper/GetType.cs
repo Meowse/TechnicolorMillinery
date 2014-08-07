441cdfa58b86510cf3aadeb4460cbe3874244970
rickfleehart <rickfleehart@gmail.com>
2014-08-06 19:09:59 -0700

    Homework6 TriangleTyper initial commit

---
+++ Rick.Fleehart/Homework6/TriangleTyper/TriangleTyper/GetType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTyper
+{
+    public class GetType(SideA SideB SideC)
+    {
+        /*
+         * TriangleTyper.GetType() is a function that returns the type of a triangle -- equilateral, isosceles, or scalene.
+
+        “equilateral” = all 3 sides are the same length
+        “isosceles” = 2 sides are the same length, and the third is different
+        “scalene” = all 3 sides are different lengths.
+
+        TriangleTyper.GetType(3, 3, 3) => should return “Equilateral”
+         * */
+
+
+    }
+}
