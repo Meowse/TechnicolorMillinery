21e124ea3e49ddd833ab4253a4fe4aead2398a85
ndcraig <nelsondcraig@gmail.com>
2014-08-07 16:32:34 -0700

    HW6: Update
    Doesn't really work.

--- Nelson.Craig/Session 6/Sorter/Sorter/SortUtility.cs
+++ Nelson.Craig/Session 6/Sorter/Sorter/SortUtility.cs
@@ -1,4 +1,7 @@
-﻿namespace Sorter
+﻿using System;
+using System.Security.Policy;
+
+namespace Sorter
 {
     public class SortUtility
     {
@@ -12,10 +15,32 @@
             // Last value in the array is "values[values.Length - 1]"
 
             // Example of using a for-loop to double all elements of an array
-            for (int i = 0; i < values.Length; i++)
-            {
-                values[i] = values[i] * 2;
+            bool hasSorted = false;
+            for (int i = 0; i < values.Length - 1; i++)
+             {
+                if (hasSorted == false)
+                {
+                    for (int k = 0; k < values.Length - 1; k++)
+                    {
+
+                            if (values[k] > values[k + 1])
+                            {
+                                int lesserValue = values[k];
+                                int greaterValue = values[k + 1];
+
+                                values[k] = greaterValue;
+                                values[k + 1] = lesserValue;
+
+                                hasSorted = true;
+                            }
+                            else
+                            {
+                                hasSorted = false;
+                            }
+                    }
+                }
             }
+             
         }
     }
 }
