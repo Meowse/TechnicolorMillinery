83865753e968b4fb8148bd4ad44638e42c66d11a
aodenwe <aaron.odenweller@gmail.com>
2014-08-07 18:11:45 -0700

    Session 6 Sort Homework

---
+++ Aaron.Odenweller/Session6_Sort_Example_Solution/Session6_Sort_Example_Project/Sorter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session6_Sort_Example_Project
+{
+    public class Sorter
+    {
+        static int count;
+
+        public void Sort(int[] values)
+        {
+           
+            //int[] newArray = new int[7];  //[7] is the SIZE of the Array.  NOT the index
+            do
+            {
+                count = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                count = i;
+                if (count != values.Length - 1)
+                {
+                
+                    if (values[i] > values[i + 1])
+                    {
+                        Swap(values, i, i + 1);
+                    }
+              
+                }
+            }
+                
+            } while (FinishedSorting(values) != true);
+        }
+
+        private void Swap(int[] values, int indexLeft, int indexRight)
+        {
+            var temp = values[indexRight];
+            values[indexRight] = values[indexLeft];
+            values[indexLeft] = temp;
+        }
+
+        private bool FinishedSorting(int[] values)
+        {
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (i == values.Length - 1)
+                {
+                    return true;
+                }
+                if (values[i + 1] < values[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Aaron.Odenweller/Session6_Sort_Example_Solution/SortThings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Session6_Sort_Example_Project;
+
+
+namespace SortThings
+{
+    class Program
+    {
+        static void Main()
+        {
+
+             var aSorter = new Sorter();
+
+            Console.WriteLine("Enter numbers to sort, or 'exit' to exit.");
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                int[] values = ParseInput(input);
+                aSorter.Sort(values);
+                DisplayOutput(values);
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static int[] ParseInput(string anInput)
+        {
+            string[] inputs = anInput.Split(new[] {" ", ","}, StringSplitOptions.RemoveEmptyEntries); // This Split method takes the User's input and then takes it apart into space and comma separated numbers
+            return inputs.Select(int.Parse).ToArray(); // The select method selects everything in the Array and then returns them.  ToArray, says take those values and turn them into an Array
+        }
+
+        private static void DisplayOutput(int[] values)
+        {
+            Console.WriteLine("Sorted: " + String.Join(", ", values));
+        }
+    }
+}
