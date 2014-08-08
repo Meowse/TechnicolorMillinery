f7b282ef431e78934d3a0319c61388170ad270df
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-08-07 20:37:24 -0700

    Lab7

---
+++ Cathy.Blanscet/Lab7/TriangleTyper/TriangleTyperUtilsTests/SimpleTriangleTyperTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using triangleTyperUtils;
+
+namespace TriangleTyperUtilsTests
+{
+    [TestFixture]
+    public class SimpleTriangleTyperTests
+    {
+        private readonly TriangleTyperClass _typer = new TriangleTyperClass();
+
+        [Test]
+        public void EqualSidesShouldBeEqualateral()
+        {
+            //TriangleTyperClass typer = new TriangleTyperClass();
+            //string actualType = _typer.GetTriangleType(1, 1, 1);
+            Assert.AreEqual("Equilateral", _typer.GetTriangleType(1, 1, 1));
+        }
+
+        [Test]
+        public void OnlyTwoSidesEqualShouldBeIsosceles()
+        {
+            //TriangleTyperClass typer = new TriangleTyperClass();
+            //string actualType = _typer.GetTriangleType(2, 2, 3);
+            Assert.AreEqual("Isosceles", _typer.GetTriangleType(2, 2, 3));
+        }
+
+        [Test]
+        public void TwoSidesEqualTheThirdIsNotTriangle()
+        {
+            //string actualType = _typer.GetTriangleType(1, 2, 3);
+            Assert.AreEqual("Not a Triangle", _typer.GetTriangleType(1, 2, 3));
+        }
+
+        [Test]
+        public void NoSidesEqualShouldBeScalene()
+        {
+            //string actualType = _typer.GetTriangleType(2, 3, 4);
+            Assert.AreEqual("Scalene", _typer.GetTriangleType(2, 3, 4));
+        }
+
+        [Test]
+        public void AnyZeroLengthSidesAreNotATrangle()
+        {
+            //string actualType = _typer.GetTriangleType(0, 0, 0);
+            Assert.AreEqual("Not a Triangle", _typer.GetTriangleType(0, 0, 0));
+        }
+
+        public void AssertAllOrderings(string expectedtype, long a, long b, long c)
+        {
+
+        }
+    }
+}
---
+++ Cathy.Blanscet/Lab7/triangleTyperUtils/TriangleTyperClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangleTyperUtils
+{
+    public class TriangleTyperClass
+    {
+        public string GetTriangleType(long a, long b, long c)
+        {
+            //throw new NotImplementedException();
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+            {
+                return "Not a Triangle";
+            }
+
+            if ((a + b <= c) || (b + c <= a) || (a + c <= b))
+            {
+                return "Not a Triangle";
+            }
+
+            if (a == b && b == c && a == c)                 
+            {//a==c is not really needed but it makes its more clear to those that don't know geometry/logic
+               return "Equilateral"; 
+            }
+
+            if (a != b && b != c && a != c)
+            {
+                return "Scalene";
+            }
+
+            return "Isosceles";
+        }
+    }
+}
