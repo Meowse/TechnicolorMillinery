2572ec8f4cb097b961209efea02943f2bad1b412
dcgutman <chrisgutman@gmail.com>
2014-08-07 22:16:54 -0500

    Initial check-in (in class)

---
+++ Chris.Gutman/Homework due 8-7/TriangleStuff/TriangleStuff/TriangleStuff/TriangleTyper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleStuff
+{
+    public class TriangleTyper
+    {
+        public string getType(long a, long b, long c)
+        {
+            if ((a == b) && (b == c) && (a >= 0))
+            {
+                return "Equilateral";
+            }
+            else if ((a + b == c) || (b + c == a) ||(c + a== b))
+            {
+                return "NotATriangle";
+            }
+            else if ((a != b) && (b != c) && (a != c))
+            {
+                return "Scalene";
+            }
+            {
+                return "";
+            }
+
+          
+
+        }
+
+    }
+}
---
+++ Chris.Gutman/Homework due 8-7/TriangleStuff/TriangleStuff/TriangleTest/SimpleTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleStuff;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        private static TriangleTyper Triangles = new TriangleTyper();
+
+        [Test]
+
+        public void EqualSidesShouldBeEquilateral()
+        {
+            Assert.AreEqual("Equilateral", Triangles.getType(1,1,1));
+        }
+
+        public void assertallorderings(string expectedtype, long a, long b, long c)
+        {
+            Assert.AreEqual(expectedtype, Triangles.getType(a, b, c));
+            Assert.AreEqual(expectedtype, Triangles.getType(a, c, b));
+            Assert.AreEqual(expectedtype, Triangles.getType(b, a, c));
+            Assert.AreEqual(expectedtype, Triangles.getType(b, c, a));
+            Assert.AreEqual(expectedtype, Triangles.getType(c, a, b));
+            Assert.AreEqual(expectedtype, Triangles.getType(c, b, a));
+
+        }
+    }
+}
