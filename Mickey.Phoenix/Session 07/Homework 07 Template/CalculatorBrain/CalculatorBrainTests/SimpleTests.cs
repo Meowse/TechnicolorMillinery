a56b5e3737266d4f07d6517cb755b72ee49656c6
Meowse <meowse@remove.gmail.remove.com>
2014-08-07 21:00:31 -0700

    Session 07 in-class work and homework

---
+++ Mickey.Phoenix/Session 07/Homework 07 Template/CalculatorBrain/CalculatorBrainTests/SimpleTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorBrain;
+using NUnit.Framework;
+
+namespace CalculatorBrainTests
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        [Test]
+        public void ShouldDisplayZeroByDefault()
+        {
+            var calculator = new Calculator();
+            Assert.AreEqual("0", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void ShouldDisplayZeroAfterClear()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('1');
+            calculator.ProvideInput('+');
+            calculator.ProvideInput('c');
+            Assert.AreEqual("0", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void ShouldDisplaySingleDigit()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('1');
+            Assert.AreEqual("1", calculator.GetDisplay());
+        }
+
+        [Test] public void ShouldAddSmallSingleDigits()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('1');
+            Assert.AreEqual("1", calculator.GetDisplay());
+            calculator.ProvideInput('+');
+            Assert.AreEqual("1", calculator.GetDisplay());
+            calculator.ProvideInput('2');
+            Assert.AreEqual("2", calculator.GetDisplay());
+            calculator.ProvideInput('=');
+            Assert.AreEqual("3", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void ShouldAddLargeSingleDigitsIntoDoubleDigits()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('9');
+            calculator.ProvideInput('+');
+            calculator.ProvideInput('8');
+            calculator.ProvideInput('=');
+            Assert.AreEqual("17", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void ShouldAcceptMultiDigitNumbers()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('3');
+            Assert.AreEqual("3", calculator.GetDisplay());
+            calculator.ProvideInput('1');
+            Assert.AreEqual("31", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void CanSubtractSingleDigitNumbers()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('7');
+            Assert.AreEqual("7", calculator.GetDisplay());
+            calculator.ProvideInput('-');
+            Assert.AreEqual("7", calculator.GetDisplay());
+            calculator.ProvideInput('2');
+            Assert.AreEqual("2", calculator.GetDisplay());
+            calculator.ProvideInput('=');
+            Assert.AreEqual("5", calculator.GetDisplay());
+        }
+
+        [Test]
+        public void CanSubtractMultiDigitNumbers()
+        {
+            var calculator = new Calculator();
+            calculator.ProvideInput('7');
+            Assert.AreEqual("7", calculator.GetDisplay());
+            calculator.ProvideInput('1');
+            Assert.AreEqual("71", calculator.GetDisplay());
+            calculator.ProvideInput('-');
+            Assert.AreEqual("71", calculator.GetDisplay());
+            calculator.ProvideInput('2');
+            Assert.AreEqual("2", calculator.GetDisplay());
+            calculator.ProvideInput('=');
+            Assert.AreEqual("69", calculator.GetDisplay());
+        }
+    }
+}
