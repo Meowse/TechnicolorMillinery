e40b111aefc22bbbbd3e1c09071bce9cd523e8a2
Helen-Cathlee <deanandcathy.wa@netzero.net>
2014-08-09 17:58:57 -0700

    Homework7 part 1 fix Triangle Typer Tests all pass

--- Cathy.Blanscet/Lab7/triangleTyperUtils/TriangleTyperClass.cs
+++ Cathy.Blanscet/Lab7/triangleTyperUtils/TriangleTyperClass.cs
@@ -11,24 +11,101 @@ namespace triangleTyperUtils
         public string GetTriangleType(long a, long b, long c)
         {
             //throw new NotImplementedException();
+
+            // put the zero and negative number tests first so that they won't match any of the other types
             if ((a <= 0) || (b <= 0) || (c <= 0))
             {
                 return "Not a Triangle";
             }
 
-            if ((a + b <= c) || (b + c <= a) || (a + c <= b))
+            // this test for valid triangle 
+            // can't handle long.MaxValue; could move it below the other tests but then might not get isosceles
+            // a try-catch is best
+            try
             {
-                return "Not a Triangle";
+                if ((a + b <= c) || (b + c <= a) || (a + c <= b))
+                {
+                    return "Not a Triangle";
+                }
+            }
+            catch
+            {
+                return HugeNumbersTest(a, b, c);
             }
 
+            //a==c is not really needed but it makes its more clear to those that don't know geometry/logic
             if (a == b && b == c && a == c)                 
-            {//a==c is not really needed but it makes its more clear to those that don't know geometry/logic
+            {
                return "Equilateral"; 
             }
 
             if (a != b && b != c && a != c)
             {
                 return "Scalene";
+            }
+
+            return "Isosceles";
+        }
+
+        private string HugeNumbersTest(long a , long b , long c )
+        {
+            if (a == b && b == c && a == c)
+            {
+                return "Equilateral";
+            }
+
+            if (a == long.MaxValue)
+            {
+                if (b <= (a/2) && c <= (a/2))
+                {
+                    return "Not a Triangle";
+                }
+
+                if ((a == b && b != c) || (a == c && a != b))
+                {
+                    return "Isosceles";
+                }
+
+                if (a != b && a != c && b != c)
+                {
+                    return "Scalene";
+                }
+            }
+
+            if (b == long.MaxValue)
+            {
+                if (a <= (b / 2) && c <= (b / 2))
+                {
+                    return "Not a Triangle";
+                }
+
+                if ((a == b && b != c) || (a == c && a != b))
+                {
+                    return "Isosceles";
+                }
+
+                if (a != b && a != c && b != c)
+                {
+                    return "Scalene";
+                }
+            }
+
+            if (c == long.MaxValue)
+            {
+                if (a <= (c / 2) && b <= (c / 2))
+                {
+                    return "Not a Triangle";
+                }
+
+                if ((a == b && b != c) || (a == c && a != b))
+                {
+                    return "Isosceles";
+                }
+
+                if (a != b && a != c && b != c)
+                {
+                    return "Scalene";
+                }
             }
 
             return "Isosceles";
