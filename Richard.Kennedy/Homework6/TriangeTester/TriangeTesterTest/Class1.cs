83920c234788e7bc5032c563581eeb2b1d27db64
RichKenn <RichKenn@myuw.net>
2014-08-10 17:01:11 -0700

    Homework 6 Triangle Tester Intermediate

---
+++ Richard.Kennedy/Homework6/TriangeTester/TriangeTesterTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TriangeTesterTest
+{
+    public class Class1
+    {
+        private readonly TriangleTyper _typer = new TriangleTyper();
+
+        [Test]
+        public void AnyNonPositiveIntegerLengthSidesAreNotTriangles()
+        {
+            string expectedType = "Not a triangle";
+            string actualType = _typer.triangleCheck(0, 0, 0);
+            Assert.AreEqual(expectedType, actualType);
+        }
+    }
+}
