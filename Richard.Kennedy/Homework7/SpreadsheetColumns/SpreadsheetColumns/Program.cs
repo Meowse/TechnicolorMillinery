b70e644e3c772daa46be982951bcc851c9b06f9e
RichKenn <RichKenn@myuw.net>
2014-08-11 21:46:03 -0700

    Homework 7 Extra Credit w/o Tests

--- Richard.Kennedy/Homework7/SpreadsheetColumns/SpreadsheetColumns/Program.cs
+++ Richard.Kennedy/Homework7/SpreadsheetColumns/SpreadsheetColumns/Program.cs
@@ -10,6 +10,26 @@ namespace SpreadsheetColumns
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a column number to be converted to a column letter, or 'exit' to exit.");
+            string input = Console.ReadLine();
+            while (input != "exit")
+            {
+                int divisor = Convert.ToInt32(input);
+                int columnNumber = divisor;
+                string columnLetter = "";
+                int modulus = 0;
+
+                while (divisor > 0)
+                {
+                    modulus = (divisor - 1) % 26;
+                    columnLetter = (char)(65 + modulus) + columnLetter;
+                    divisor = (int)((divisor - modulus) / 26);
+                }
+                //return columnLetter;
+                Console.WriteLine("Column " + columnNumber + " is column letter " + columnLetter + "\n");
+                input = Console.ReadLine();
+            } 
         }
+
     }
 }
