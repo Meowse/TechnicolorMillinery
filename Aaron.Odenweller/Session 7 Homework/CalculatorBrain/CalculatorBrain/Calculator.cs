ebf62c143388fe6fdd51f02dff1ed9c830205891
aodenwe <aaron.odenweller@gmail.com>
2014-08-11 22:04:06 -0700

    Session 7 Calculator Baseline HW

---
+++ Aaron.Odenweller/Session 7 Homework/CalculatorBrain/CalculatorBrain/Calculator.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CalculatorBrain
+{
+    public class Calculator
+    {
+        private decimal number;
+        private string display;
+        private char operation;
+
+
+        public bool DecimalValueEntered(string value)
+        {
+
+            bool decimalEntered = true;
+
+            try
+            {
+                decimal.Parse(value);
+
+            }
+            catch (Exception)
+            {
+                decimalEntered = false;
+            }
+
+            return decimalEntered;
+
+        }
+
+        public void setNumber(string inputNumber)
+        {
+            this.number = decimal.Parse(inputNumber);
+        }
+
+        // The current state of the calculator will have to be stored somehow
+        // in instance variables, here, declared directly inside the "Calculator"
+        // scope.
+
+        /*
+         * Valid inputs:
+         * '0' -- input the digit 0
+         * ...
+         * '9' -- input the digit 9
+         * '+' -- add
+         * '-' -- subtract
+         * '*' -- multiply
+         * '/' -- divide
+         * '=' -- perform the current calculation
+         * 'c' -- clear
+         */
+
+        public void setOperation(string aOperation)
+        {
+
+            this.operation = Convert.ToChar(aOperation.Trim());
+        }
+
+        public void PerformOperation(string input)
+        {
+
+            switch (this.operation)
+            {
+                case '+':
+                    AddNumbers(decimal.Parse(input));
+                    //ClearDisplay();
+                    SetDisplay(" + " + input);
+                    break;
+
+                case '-':
+                    SubtractNumbers(decimal.Parse(input));
+                    //ClearDisplay();
+                    SetDisplay(" - " + input);
+                    break;
+
+                case '*':
+                    MultiplyNumber(decimal.Parse(input));
+                    //ClearDisplay();
+                    SetDisplay(" * " + input);
+                    break;
+
+                case '/':
+                    DivideNumbers(decimal.Parse(input));
+
+                    break;
+
+                case '=':
+                    //ClearDisplay();
+                    SetDisplay("The value is now: " + number.ToString());
+                    break;
+
+                case 'c':
+                    ClearMemory();
+                    ClearDisplay();
+                    //SetDisplay("This has been cleared.  The value of the calculator is now: " + number.ToString());
+                    break;
+
+                default:
+                    ClearDisplay();
+                    SetDisplay("Enter a Valid Character");
+                    break;
+            }
+        }
+
+        public void SetDisplay(String setDisplayValue)
+        {
+            display += setDisplayValue;
+        }
+
+
+        public string GetDisplay(Calculator calcObject)
+        {
+
+            return calcObject.display;
+        }
+
+        public void ClearMemory()
+        {
+         this.number = 0;
+        }
+        public void ClearDisplay()
+        {
+            this.display = "";
+        }
+
+        public void AddNumbers(decimal secondNumber)
+        {
+            decimal tempDecimal = this.number;
+            this.number = tempDecimal + secondNumber;
+        }
+
+        public void SubtractNumbers(decimal secondNumber)
+        {
+            decimal tempDecimal = this.number;
+            this.number = tempDecimal - secondNumber;
+        }
+
+        public void DivideNumbers(decimal secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                decimal tempDecimal = this.number;
+                this.number = tempDecimal/secondNumber;
+                //ClearDisplay();
+                SetDisplay(" / " + secondNumber);
+            }
+
+            else
+            {
+                ClearDisplay();
+                SetDisplay("Cannot Divide By Zero");
+            }
+        }
+
+        public void MultiplyNumber(decimal secondNumber)
+        {
+            decimal tempDecimal = this.number;
+            this.number = tempDecimal * secondNumber;
+        }
+    }
+}
---
+++ Aaron.Odenweller/Session 7 Homework/CalculatorBrain/CalculatorConsoleApplication/CalculatorConsole.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorBrain;
+
+namespace CalculatorConsoleApplication
+{
+    class CalculatorConsole
+    {
+        private static String _input;
+
+        static void Main(string[] args)
+        {
+            Calculator aCalculator = new Calculator();
+            aCalculator.SetDisplay("Type 'Exit' to exit." + " Other than 'Exit', valid inputs are: digits 0 - 9, '+','-','*','/','=', or 'c' ");
+            Console.WriteLine(aCalculator.GetDisplay(aCalculator));
+            aCalculator.ClearDisplay();
+ 
+            do
+            {
+              _input = Console.ReadLine().Trim();  //will bomb if null
+                
+                if (_input != "Exit")
+                {
+                    performNumberVerification(aCalculator, _input);
+                    aCalculator.setNumber(_input);
+                    aCalculator.SetDisplay(_input);
+                    Console.WriteLine(aCalculator.GetDisplay(aCalculator) + " was entered.  What operation do you want to perform?");
+                    
+                    do
+                    {
+                        _input = Console.ReadLine().Trim();
+                        validateOperator(aCalculator);
+                        aCalculator.setOperation(_input);
+                        if (_input != "c" && _input != "=")
+                        {
+                            Console.WriteLine("Enter a Number");
+                            _input = Console.ReadLine().Trim();
+                            performNumberVerification(aCalculator, _input);
+                        }
+
+                        aCalculator.PerformOperation(_input);
+                        Console.WriteLine("Operations: " + aCalculator.GetDisplay(aCalculator) + ". What do you want to do next?");
+                    } while (_input != "Exit");
+
+
+                    //aCalculator.Operation(_input);
+                    //take value and wait for User input to perform operation
+
+                    //var response = TriangleTyper.GetTriangleType(long.Parse(arrayOfInputs[0]), long.Parse(arrayOfInputs[1]), long.Parse(arrayOfInputs[2]));
+                    //Console.WriteLine(response);
+                }
+                
+            } while (_input != "Exit");
+
+        }
+
+        private static void performNumberVerification(Calculator aCalculator, String input)
+        {
+            bool validClearOperator = input.Equals("c");
+            bool digitEntered = aCalculator.DecimalValueEntered(_input);
+            while (digitEntered == false && _input != "Exit" && validClearOperator == false)
+            {
+                Console.WriteLine("Decimal Number Only");
+                _input = Console.ReadLine().Trim();
+                digitEntered = aCalculator.DecimalValueEntered(_input);
+
+            }
+             
+        }
+
+        private static void validateOperator(Calculator tempCalculator)
+        {
+           
+            while (_input != "Exit" && checkOperator(_input) == false)
+            {
+                Console.WriteLine("Please enter valid operator. Either: '+','-','*','/','=', or 'c'");
+                Console.WriteLine(tempCalculator.GetDisplay(tempCalculator));
+
+                _input = Console.ReadLine().Trim();
+                
+            }
+
+        }
+
+        private static bool checkOperator(string input)
+        {
+            bool stop = false;
+            char temp = Convert.ToChar(input);
+
+            switch (temp)
+            {
+
+                case '+':
+                    stop = true;
+                    break;
+
+                case '-':
+                    stop = true;
+                    break;
+
+                case '*':
+                    stop = true;
+                    break;
+
+                case '/':
+                    stop = true;
+                    break;
+
+                case '=':
+                    stop = true;
+                    break;
+
+                case 'c':
+                    stop = true;
+                    break;
+
+                default:
+                    stop = false;
+                    break;
+                  
+            }
+
+            return stop;
+        } 
+
+     }
+}
