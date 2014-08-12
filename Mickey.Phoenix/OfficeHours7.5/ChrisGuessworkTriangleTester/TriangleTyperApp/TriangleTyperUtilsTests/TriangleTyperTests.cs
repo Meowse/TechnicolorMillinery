2189b2d8c7e61ae27c74196fc7a671b0a6552bf1
Meowse <meowse@remove.gmail.remove.com>
2014-08-11 18:53:13 -0700

    Office Hours 7.5 Chris's problem best guess baseline

---
+++ Mickey.Phoenix/OfficeHours7.5/ChrisGuessworkTriangleTester/TriangleTyperApp/TriangleTyperUtilsTests/TriangleTyperTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleTyperUtils;
+
+namespace TriangleTyperUtilsTests
+{
+    [TextFixture]
+    public class TriangleTyperTests
+    {
+        private readonly TriangleTyper _typer = new TriangleTyper();
+
+        [Test]
+        public void ShouldReturnEquilateralForThreeEqualSides()
+        {
+            Assert.AreEqual("Equilateral", _typer.GetTriangleType(1, 1, 1));
+        }
+    }
+}
---
+++ Mickey.Phoenix/OfficeHours7.5/ChrisGuessworkTriangleTester/TriangleTyperUtils/TriangleTyper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTyperUtils
+{
+    public class TriangleTyper
+    {
+        public string GetTriangleType(int a, int b, int c)
+        {
+            return "Equilateral";
+        }
+    }
+}
