3c4a8c459d4335604701f3c46451bc713c4d5398
dcgutman <chrisgutman@gmail.com>
2014-08-12 21:10:09 -0500

    Adding Initial event code to calculator form

--- Chris.Gutman/Homework7/Homework 07 Template/CalculatorBrain/WindowsFormsApplication1/Form1.Designer.cs
+++ Chris.Gutman/Homework7/Homework 07 Template/CalculatorBrain/WindowsFormsApplication1/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_click);
             // 
             // button2
             // 
@@ -62,6 +63,7 @@
             this.button2.TabIndex = 0;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_click);
             // 
             // button3
             // 
@@ -71,6 +73,7 @@
             this.button3.TabIndex = 0;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_click);
             // 
             // button4
             // 
@@ -80,6 +83,7 @@
             this.button4.TabIndex = 0;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_click);
             // 
             // button5
             // 
@@ -89,6 +93,7 @@
             this.button5.TabIndex = 0;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_click);
             // 
             // button6
             // 
@@ -98,6 +103,7 @@
             this.button6.TabIndex = 0;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_click);
             // 
             // button7
             // 
@@ -107,6 +113,7 @@
             this.button7.TabIndex = 0;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_click);
             // 
             // button8
             // 
@@ -116,6 +123,7 @@
             this.button8.TabIndex = 0;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_click);
             // 
             // button9
             // 
@@ -125,6 +133,7 @@
             this.button9.TabIndex = 0;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_click);
             // 
             // button10
             // 
@@ -134,6 +143,7 @@
             this.button10.TabIndex = 0;
             this.button10.Text = ".";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Point_click);
             // 
             // button11
             // 
@@ -143,6 +153,7 @@
             this.button11.TabIndex = 0;
             this.button11.Text = "0";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button0_click);
             // 
             // minus
             // 
--- Chris.Gutman/Homework7/Homework 07 Template/CalculatorBrain/WindowsFormsApplication1/Form1.cs
+++ Chris.Gutman/Homework7/Homework 07 Template/CalculatorBrain/WindowsFormsApplication1/Form1.cs
@@ -2,19 +2,97 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CalculatorBrain;
+using System.Text.RegularExpressions;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Calculator _calculator = new Calculator();
+
+        private RegexOptions reoptions = new RegexOptions();
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Button1_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('1');
+        }
+
+        private void Button2_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('2');
+        }
+
+        private void Button3_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('3');
+        }
+
+        private void Button4_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('4');
+        }
+
+        private void Button5_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('5');
+        }
+
+        private void Button6_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('6');
+        }
+
+        private void Button7_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('7');
+        }
+
+        private void Button8_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('8');
+        }
+
+        private void Button9_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('9');
+        }
+
+        private void Button0_click(object sender, EventArgs e)
+        {
+            _calculator.ProvideInput('0');
+        }
+
+
+
+        private void Point_click(object sender, EventArgs e)
+        {
+            const string mystring = @".+\..+";
+            reoptions = RegexOptions.Singleline | RegexOptions.IgnoreCase;
+            var re = new Regex(mystring);
+            var rematch = re.Match(mystring);
+
+            if (rematch.Length >= 1)
+            {
+                
+            }
+            else
+            {
+                _calculator.ProvideInput('.');
+            }
+        }
     }
+   
 }
