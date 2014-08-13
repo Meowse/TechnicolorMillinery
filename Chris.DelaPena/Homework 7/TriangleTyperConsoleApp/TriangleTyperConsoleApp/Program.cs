6a9e964843bbf08b3e94561e815c3a30b5134ce7
cdelapena <delapc@gmail.com>
2014-08-12 21:26:35 -0700

    Homework 7 update

--- Chris.DelaPena/Homework 7/TriangleTyperConsoleApp/TriangleTyperConsoleApp/Program.cs
+++ Chris.DelaPena/Homework 7/TriangleTyperConsoleApp/TriangleTyperConsoleApp/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriangleTyperUtils;
 
 namespace TriangleTyperConsoleApp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            TriangleTyper typer = new TriangleTyper();
+            Triangle triangle = GetTriangle();
+            while (triangle != null)
+            {
+                Console.WriteLine("Triangle typer returned {0} for triangle {1}.",
+                    typer.GetTriangleType(triangle.A, triangle.B, triangle.C))
+                ;
+                triangle
+                ;
+            }
+        }
+
+        private static Triangle GetTriangle()
+        {
+            //Console.WriteLine("Please enter a triangle with whole-number sides:");
+            //string inputs = Console.ReadLine();
+            //inputs = inputs.Split(new string[]
+    return new Triangle();
+            }
+        }
+
+        internal class Triangle
+        {
+            public long A;
+            public long B;
+            public long C;
         }
     }
 }
--- Chris.DelaPena/Homework 7/TriangleTyperConsoleApp/TriangleTyperUtils/TriangleTyper.cs
+++ Chris.DelaPena/Homework 7/TriangleTyperConsoleApp/TriangleTyperUtils/TriangleTyper.cs
@@ -10,7 +10,33 @@ namespace TriangleTyperUtils
     {
         public string GetTriangleType(long a, long b, long c) //change from int to long, go from ii to alphanumeric
         {
-            return "Equilateral";
+
+            //need several or commands
+            //check greater than zero
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return "NotATriangle";
+            }
+            //for add up, do <=
+            if ((a + b <= c) || (b + c <= a) || (c + a <= b))
+            {
+                return "NotATriangle";
+            }
+
+            //Make a return statement for isoscoles
+            if (a == b && b == c && c == a)
+            {
+                return "Equilateral";
+            }
+
+            //return scalene if a!=b and b!=c, but you need more (or fails isoscoles
+            //inclue a !=c
+            if (a != b && b != c && a != c)
+            {
+                return "Scalene";
+            }
+            //if a == b || b == c || c == a return isoscoles
+            return "Isoscoles";
         }
     }
 }
