826f888107fdd6d4908d923d04b5591fc6ca78b4
tryon12 <tryon12@hotmail.com>
2014-08-14 00:26:45 -0700

    Updated CalBrain Logic

--- Tryon.Yamaguchi/Session 07/CalculatorBrain/CalculatorBrain/Calculator.cs
+++ Tryon.Yamaguchi/Session 07/CalculatorBrain/CalculatorBrain/Calculator.cs
@@ -8,6 +8,10 @@ namespace CalculatorBrain
 {
     public class Calculator
     {
+        private string _currentValue = "";
+        private decimal _totalValue;
+        private char _mathSymbol;
+
         // The current state of the calculator will have to be stored somehow
         // in instance variables, here, declared directly inside the "Calculator"
         // scope.
@@ -26,11 +30,142 @@ namespace CalculatorBrain
          */
         public void ProvideInput(char input)
         {
+            switch (input)
+            {
+                case 'C':
+                    _currentValue = "0";
+                    _totalValue = 0;
+                    break;
+                case '1':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '2':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '3':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '4':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '5':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '6':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '7':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '8':
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '.':
+                    if (_currentValue.Contains("."))
+                    {
+                        //Do nothing because 2nd decimal is invalid
+                    }
+                    else
+                    {
+                        _currentValue = _currentValue + input.ToString();
+                    }
+
+                    break;
+                case '0':
+                    decimal checkZero = decimal.Parse(_currentValue);
+
+                    if (checkZero > 0)
+                    {
+                        _currentValue = _currentValue + input.ToString();
+                    }
+                    break;
+                case '+':
+
+                    if (_mathSymbol == '+')
+                    {
+                        _totalValue = _totalValue + convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                    }
+                    else
+                    {
+                        _totalValue = convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                        _mathSymbol = '+';
+                        
+                    }
+                   
+                    break;
+                case '-':
+
+                    if (_mathSymbol == '-')
+                    {
+                        _totalValue = _totalValue - convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                    }
+                    else
+                    {
+                        _totalValue = convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                        _mathSymbol = '-';
+
+                    }
+                    
+                    break;
+                case '*':
+                    if (_mathSymbol == '*')
+                    {
+                        _totalValue = _totalValue * convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                    }
+                    else
+                    {
+                        _totalValue = convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                        _mathSymbol = '*';
+
+                    }
+                   
+                    break;
+                case '/':
+                    if (_mathSymbol == '/')
+                    {
+                        _totalValue = _totalValue / convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                    }
+                    else
+                    {
+                        _totalValue = convertValueToDecimal(_currentValue);
+                        _currentValue = "";
+                        _mathSymbol = '/';
+
+                    }
+                  
+                    break;
+                case '=':
+
+                    _totalValue = _totalValue + convertValueToDecimal(_currentValue);
+                    _currentValue = _totalValue.ToString();
+                    break;
+                default:
+                    break;
+
+
+            }
+   
+            
         }
 
         public string GetDisplay()
         {
-            return "0";
+            decimal currentValueAsDecimal = decimal.Parse(_currentValue);
+            return currentValueAsDecimal.ToString();
+        }
+
+        public decimal convertValueToDecimal(string currentValue)
+        {
+            decimal convertedValue = decimal.Parse(currentValue);
+            return convertedValue;
+
         }
     }
 }
