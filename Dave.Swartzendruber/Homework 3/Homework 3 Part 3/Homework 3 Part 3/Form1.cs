2af8dec4d1bb05313dd1b3cc23e63d08db7eb0c6
davidswa08 <dswartz@uw.edu>
2014-08-13 20:11:24 -0700

    Home Work 3 Part 3

---
+++ Dave.Swartzendruber/Homework 3/Homework 3 Part 3/Homework 3 Part 3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_3_Part_3
+{
+    public partial class Form1 : Form
+    {
+        private int addToText;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addToText++;
+            displayNumber.Text = addToText.ToString();
+            if (addToText >= 10)
+            {
+                MessageBox.Show("Number too big!");
+                addToText = 0;
+
+
+            }
+        }
+    }
+}
