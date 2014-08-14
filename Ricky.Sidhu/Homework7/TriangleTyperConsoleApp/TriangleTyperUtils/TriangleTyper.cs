4561b0a8a04041ba9c40aac769cb677c8be10c6e
ricksi <sidhu_rick20@yahoo.ca>
2014-08-14 12:55:50 -0700

    Homework 7 TriangleTyper
    Took a while but now I think I somewhat understand it

---
+++ Ricky.Sidhu/Homework7/TriangleTyperConsoleApp/TriangleTyperUtils/TriangleTyper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTyperUtils
+{
+    public class TriangleTyper
+    {
+        public string GetTriangleType(long a, long b, long c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return "NotATriangle";
+            }
+
+            if (((ulong)a + (ulong)b <=(ulong)c) || ((ulong)b + (ulong)c <= (ulong)a) || ((ulong)c +(ulong)a <= (ulong)b))
+            {
+                return "NotATriangle";
+            }
+
+            if (a == b && b == c && c == a)
+            {
+                return "Equilateral";
+            }
+
+            if (a != b && b != c && a != c)
+            {
+                return "Scalene";
+            }
+
+            return "Isosceles";
+        }
+
+// For the contemplation of fundamental equivalence:
+//            if (a == b || b == c || c == a)
+//            {
+//                return "Isosceles";
+//            }
+//            return "Scalene";
+    }
+}
