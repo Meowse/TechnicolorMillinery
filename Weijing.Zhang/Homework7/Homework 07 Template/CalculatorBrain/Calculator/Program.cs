82cd1c937c2488bcfdc9dc4532960239d672615e
weijingzhang <zhangiva@gmail.com>
2014-08-13 23:16:27 -0700

    Homework 7 part2(CalculatorBrain)

---
+++ Weijing.Zhang/Homework7/Homework 07 Template/CalculatorBrain/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorBrain;
+
+namespace ConcoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Calculator Usage:");
+            Console.WriteLine("\t Type in your expression, result will be displayed after typing '='");
+            Console.WriteLine("\t Use 'c' to clear, 'x' to exit");
+            var calculator = new Calculator();
+            Console.Write(calculator.GetDisplay());
+
+            while (true)
+            {
+                var keyInfo = Console.ReadKey(true);
+                calculator.ProvideInput(keyInfo.KeyChar);
+                if (keyInfo.KeyChar == 'x' || keyInfo.KeyChar == 'X') break;
+
+                int numOfChar = Console.CursorLeft;
+                for (int i = 0; i < numOfChar; i++) Console.Write("\b \b");
+                Console.Write(calculator.GetDisplay());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- Weijing.Zhang/Homework7/Homework 07 Template/CalculatorBrain/CalculatorBrain/Calculator.cs
+++ Weijing.Zhang/Homework7/Homework 07 Template/CalculatorBrain/CalculatorBrain/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,11 @@ namespace CalculatorBrain
         // The current state of the calculator will have to be stored somehow
         // in instance variables, here, declared directly inside the "Calculator"
         // scope.
+
+        private string number1 = "";
+        private string op = "";
+        private string number2 = "";
+        private string currentDisplay = "0";
 
         /*
          * Valid inputs:
@@ -26,11 +32,79 @@ namespace CalculatorBrain
          */
         public void ProvideInput(char input)
         {
+            if (input == 'c' || input == 'C')
+            {
+                ClearMemory();
+                currentDisplay = "0";
+                return;
+            }
+
+            if (IsNumber(input))
+            {
+                if (op == "")
+                {
+                    number1 = number1 + input;
+                    currentDisplay = ulong.Parse(number1).ToString();
+                }
+                else
+                {
+                    number2 = number2 + input;
+                    currentDisplay = ulong.Parse(number2).ToString();
+                }
+            }
+            else if(input == '+' || input == '-' || input =='*' || input == '/')
+            {
+                op = input.ToString();
+            }
+            else if (input == '=')
+            {
+                if (op == "" || number1 == "" || number2 == "")
+                {
+                    currentDisplay = "Invalid expression";
+                }
+                else
+                {
+                    switch (op)
+                    {
+                        case "+":
+                            currentDisplay = (ulong.Parse(number1) + ulong.Parse(number2)).ToString();
+                            break;
+                        case "-":
+                            currentDisplay = (ulong.Parse(number1) - ulong.Parse(number2)).ToString();
+                            break;
+                        case "*":
+                            currentDisplay = (ulong.Parse(number1) * ulong.Parse(number2)).ToString();
+                            break;
+                        case "/":
+                            currentDisplay = (ulong.Parse(number1) / ulong.Parse(number2)).ToString();
+                            break;
+                    }
+                }
+                ClearMemory();
+            }
+            else
+            {
+                currentDisplay = "Invalid operator: " + input;
+                ClearMemory();
+            }
         }
 
         public string GetDisplay()
         {
-            return "0";
+            return currentDisplay;
+        }
+
+        private void ClearMemory()
+        {
+            number1 = "";
+            number2 = "";
+            op = "";
+        }
+
+        private bool IsNumber(char c)
+        {
+            if (c < '0' || c > '9') return false;
+            else return true;
         }
     }
 }
