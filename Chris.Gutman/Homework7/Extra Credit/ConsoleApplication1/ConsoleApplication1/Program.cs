f6ef9466a361d204b2ee66c8e1b0dcf906e42703
dcgutman <chrisgutman@gmail.com>
2014-08-14 20:18:28 -0500

    Correcting Issues, HW7 extra credit

--- Chris.Gutman/Homework7/Extra Credit/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Chris.Gutman/Homework7/Extra Credit/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,11 +9,29 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        private static Int64[] header = new Int64[5];
 
         private static void Main(string[] args)
         {
-            Int64[] larray = {1,26,676,17576,456976}; //26^4, 26^3, 26^2, 26^1, 26^0
-            Int64[] header = new Int64[5];
+
+
+            while (true)
+            {
+                Console.WriteLine("Please enter an integer to convert to a spreadsheet column: \n");
+                Int64 n = Convert.ToInt64(Console.ReadLine());
+
+                string outputstring = getcolumn(n);
+
+                Console.WriteLine(header[4] + " ," + header[3] + " ," + header[2] + " ," + header[1] + " ," + header[0]);
+                Console.WriteLine(outputstring);
+
+            }
+        }
+
+        public static string getcolumn(Int64 n)
+        {
+            Int64[] larray = { 1, 26, 676, 17576, 456976 }; //26^4, 26^3, 26^2, 26^1, 26^0
+
             string[] names = new string[27]
             {
                 "", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
@@ -22,53 +40,49 @@ namespace ConsoleApplication1
             string mystring;
             var J = 0;
             var i = 0;
+            bool finished_f = false;
 
-// Basic principle of operation: use modulo arithmatic. 
+            // Basic principle of operation: use modulo arithmatic. 
             //larray is used for the corresponding division (base 26).
             //the remainder is factored again in the next pass of the loop.
             //the quotient is used as an index into the names array to determine the column entries.
             //use int64 to simplify cast typing and for consistancy with methods available for readline.
-            //i = 10 forces exit from the  loop.
-            //second and thrid if loops remove A's prefixed to the result.
 
-            while (true)
+            for (i = 4; i >= 0; i--)
             {
-                Console.WriteLine("Please enter an integer to convert to a spreadsheet column: \n ");
-                Int64 n = Convert.ToInt64(Console.ReadLine());
                 
-                for (i = 4; i >= 0; i--)
+                if ((Convert.ToInt64(larray[i])) <= n)
                 {
-                    if ((Convert.ToInt64(larray[i])) < n)
+                    header[i] = (n-1) / (larray[i]);
+                    n = (n - (header[i]) * (larray[i]));
+                    if (n == 0)
                     {
-                        header[i] = n / (larray[i]);
-                        n = (n - header[i] * (larray[i]));
-                    }
-                    else if ((Convert.ToInt64(larray[i])) == n)
-                    {
-                        header[i] = larray[i];
-                        n = 0;
-                    }
-                    else
-                    {
-                        header[i] = -1;
+                        finished_f = true;
                     }
                 }
-
-                mystring = "";
-
-                for (i = 4; i>=0; i--)
+                else if (finished_f == true) //((Convert.ToInt64(larray[i])) == n)
                 {
-                    if (header[i] != -1)
-                    {
-                        mystring = mystring + names[header[i]];    
-                    }
-                    
+                    header[i] = larray[i];
+                    n = 0;
+                    //finished_f = true;
                 }
+                else
+                {
+                    header[i] = -1;
+                }
+            }
 
-                Console.WriteLine(header[0] + " ," + header[1] + " ," + header[2] + " ," + header[3] + " ," + header[4]);
-                Console.WriteLine(mystring);
+            mystring = "";
+
+            for (i = 4; i >= 0; i--)
+            {
+                if (header[i] != -1)
+                {
+                    mystring = mystring + names[header[i]];
+                }
 
             }
-        } 
+            return mystring;
+        }
     }
 }
