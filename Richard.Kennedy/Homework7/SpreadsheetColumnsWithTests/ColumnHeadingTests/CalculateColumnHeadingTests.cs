6f6fcc8ad3b86da20793399161065ffc758c3dee
Meowse <meowse@remove.gmail.remove.com>
2014-08-14 21:10:14 -0700

    Post-extraction cleanup

---
+++ Richard.Kennedy/Homework7/SpreadsheetColumnsWithTests/ColumnHeadingTests/CalculateColumnHeadingTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SpreadsheetColumns;
+
+namespace ColumnHeadingTests
+{
+    [TestFixture]
+    public class CalculateColumnHeadingTests
+    {
+        [Test]
+        public void ShouldReturnAForOne()
+        {
+            Assert.AreEqual("A", CalculateColumnLetter.GetColumnHeading(1));
+        }
+
+        [Test]
+        public void ShouldReturnZFor26()
+        {
+            Assert.AreEqual("Z", CalculateColumnLetter.GetColumnHeading(26));
+        }
+
+        [Test]
+        public void ShouldReturnAAFor27()
+        {
+            Assert.AreEqual("AA", CalculateColumnLetter.GetColumnHeading(27));
+        }
+
+    }
+}
--- Richard.Kennedy/Homework7/SpreadsheetColumnsWithTests/SpreadsheetColumns/CalculateColumnLetter.cs
+++ Richard.Kennedy/Homework7/SpreadsheetColumnsWithTests/SpreadsheetColumns/CalculateColumnLetter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SpreadsheetColumns
+﻿namespace SpreadsheetColumns
 {
     public class CalculateColumnLetter
     {
@@ -12,18 +6,15 @@ namespace SpreadsheetColumns
         {
             int divisor = columnNumber;
             string columnLetter = "";
-            int modulus = 0;
 
             while (divisor > 0)
             {
-                modulus = (divisor - 1)%26;
+                int modulus = (divisor - 1)%26;
                 columnLetter = (char) (65 + modulus) + columnLetter;
-                divisor = (int) ((divisor - modulus)/26);
+                divisor = (divisor - modulus)/26;
             }
-            //return columnLetter;
 
-            string columnHeading = columnLetter;
-            return columnHeading;
+            return columnLetter;
         }
     }
 }
