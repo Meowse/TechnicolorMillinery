924027502973f128ebf51211b1df7d583ce88ab2
rickfleehart <rickfleehart@gmail.com>
2014-08-16 12:20:42 -0700

    Session8 SpreadSheet initial commit
    Also some moving of files around.

---
+++ Rick.Fleehart/Session8/SpreadSheet/SpreadsheetTests/AllTheTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SpreadsheetTests
+{
+    [TestFixture]
+
+    public class AllTheTests
+    {
+        [Test]
+        public void ShouldReturnA()
+        {
+            var calculator = new CalculatorBrainGuts.CalculatorBrain();
+            Assert.AreEqual("A", calculator.GetDisplay());
+        }  
+    }
+}
