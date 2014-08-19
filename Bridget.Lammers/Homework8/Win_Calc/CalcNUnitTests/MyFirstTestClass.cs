346d75c7a730d883c47ef2a470d495f320b112a4
lammersb <bj_lammers@msn.com>
2014-08-18 18:42:40 -0700

    Homework 8 Basic Calculator with Test Cases started
    Basic calculator, not perfect, still needs improvements, good start.

---
+++ Bridget.Lammers/Homework8/Win_Calc/CalcNUnitTests/MyFirstTestClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace CalcNUnitTests
+{
+    [TestFixture]
+    public class MyFirstTestClass
+    {
+
+        //[Test]
+        //public void ShouldDisplayZeroByDefault()
+        //{
+        //    var calculator = new Calculator();
+        //    Assert.AreEqual("0", calculator.GetDisplay());
+        //}
+
+        //[Test]
+        //public void ShouldDisplayZeroAfterClear()
+        //{
+        //    var calculator = new Calculator();
+        //    calculator.ProcessInput('1');
+       //     calculator.ProcessInput('+');
+        //    calculator.ProcessInput('c');
+        //    Assert.AreEqual("0", calculator.GetDisplay());
+        //}
+
+
+        public void Test_Add()
+        {
+            // Define the values to be used during the test execution.  
+            int a = 10;
+            int b = 20;
+            int expectedTotal = (a + b);
+
+            // Instantiate the calculator.  
+            Calculator calculator = new Calculator();
+
+            // Call the Add funcionality from the Calculator object.  
+            int actualTotal = calculator.Add(a, b);
+
+            // Check if the total obtained is correct.  
+            Assert.AreEqual(expectedTotal, actualTotal, "The Add functionality is not working correctly.");
+        }  
+
+        [Test]
+        public void PositiveTest()
+        {
+            int x = 7;
+            int y = 7;
+            Assert.AreEqual(x,y);
+
+
+        }
+
+        [Test]
+        public void NegativeTest()
+        {
+            if (true)
+                Assert.Fail("This failed, Oh No!");
+        }
+
+
+        [Test, ExpectedException(typeof (NotSupportedException))]
+        public void ExpectedExceptionTest()
+        {
+            throw new NotSupportedException();
+
+        }
+
+
+        [Test, Ignore]
+        public void NotImplementedException()
+        {
+            throw new NotImplementedException();
+
+
+        }
+
+    }
+}
