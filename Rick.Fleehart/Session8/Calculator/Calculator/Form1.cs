3dc811e89b1e7e328b712fbd05772818f8e1aa89
rickfleehart <rickfleehart@yahoo.com>
2014-08-23 09:23:40 -0700

    Session8 Calculator work in progress Sat. morning
    displays input but does not calculate yet

--- Rick.Fleehart/Session8/Calculator/Calculator/Form1.cs
+++ Rick.Fleehart/Session8/Calculator/Calculator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -18,10 +19,10 @@ namespace Calculator
             InitializeComponent();
         }
         
-        private char firstInput;   //first number entered
-        private char secondInput;  //Second number entered
+        private char _firstInput = '0';   //first number entered
+        private char _secondInput = '0';  //Second number entered
 
-        private char Operator;  // the + - * / c ce operators
+        private char _operator = '0';  // the + - * / c ce operators
         private string _runningTotal;  //Running Total as the calculator goes.
 
         private string _theRunningDisplay;
@@ -29,6 +30,7 @@ namespace Calculator
         
         public void DisplayToScreen(char theInput)
         {
+           
             // put theInput into the field in the form
             DisplayResults.Text = theInput.ToString();
         }
@@ -56,30 +58,113 @@ namespace Calculator
         public void ClearRunningDisplay()
         {
             // clears the field in the form and the running variable
-            RunningDisplay.Text = "";
+            RunningDisplay.Text = "0";
             _theRunningDisplay = ""; //clear the running display variable
         }
 
         public void ClearDisplayResults()
         {
             // clears the the bottom input field in
-            DisplayResults.Text = "";
+            DisplayResults.Text = "0";
         }
 
-        public void DoStuffToinput(char theInput)
-        {
-            if (theInput.Equals('c'))
-            {
-                ClearDisplayResults();
-                ClearRunningDisplay();
 
+        public void DealWithNumbers(char theInput)
+        {
+            if (_firstInput.Equals('0'))
+            {
+                //if this is the first number entered, then fill the _firstInput variable
+                _firstInput = theInput; //first number entered
             }
             else
             {
-                DisplayToScreen(theInput);
-                DisplayToRunningDisplay(theInput);
+                //if this is the second number entered, then fill the _secondInput variable
+                 _secondInput = theInput;  //Second number entered
             }
+            
            
+        }
+
+        public void DealWithEqualSign()
+        {
+            decimal _calculatedValue = 9; //private by default to hold the calculation
+
+
+            // show the calculated output in DisplayResults
+            DisplayResults.Text = _calculatedValue.ToString();
+
+            // put the equals sign then [calculated results] in RunningDisplay
+            RunningDisplay.Text = "=";
+
+            // put the [calculated results] in RunningDisplay -only works for single calculated values, so far
+            // to deal with 2 or three digit results loop through the chars of the [calculated results]
+            RunningDisplay.Text = _calculatedValue.ToString();
+
+            //after calculating, clean-up: clear the first and second variables
+            _firstInput = '0'; //first number entered
+            _secondInput = '0';  //Second number entered
+            _operator = '0';
+
+        }
+
+
+        public void DoStuffToinput(char theInput)
+        {
+            switch (theInput)
+            {
+                // do not show operators on screen but set the _operator switch to the operator
+                case '+':
+                case '-':
+                case 'x':
+                case '/':
+                    _operator = theInput; // 
+                    DisplayToRunningDisplay(theInput); 
+                    // display the operator in the running display
+                    // do not show operators in RunningDisplay or the results screen
+                    break;
+
+                case '=':
+                    // the equal sign calculates everything
+                    DealWithEqualSign();
+                    break;
+
+                case 'c':
+                    //Clear entry
+                    ClearDisplayResults();
+                    ClearRunningDisplay();
+                    break;
+
+                case 'e':
+                    // -------------------- not functioning yet ------------------
+                    // CE on the button, this should remove only the last char  
+                    ClearDisplayResults();
+                    ClearRunningDisplay();
+                    break;
+
+                // do stuff with the numbers
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+
+                    DisplayToScreen(theInput);
+                    DisplayToRunningDisplay(theInput);
+                    break;
+
+
+                // if it gets here then just display it for debugging
+                default:
+                    
+                    DisplayToScreen(theInput);
+                    DisplayToRunningDisplay(theInput);
+                    break;
+            }
       
         }
 
@@ -175,6 +260,7 @@ namespace Calculator
 
         private void buttonCE_Click(object sender, EventArgs e)
         {
+            // this is Shows CE on the button
             DoStuffToinput('e');
         }
 
