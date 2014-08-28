21cf95d8558716f474c569734059be20a8690819
tryon12 <tryon12@hotmail.com>
2014-08-28 00:32:39 -0700

    HW 9
    Made it worse.  Trying to code for too many conditions.

---
+++ Tryon.Yamaguchi/Session 09/Calculator Multiple Math/CalculatorBrain/CalculatorBrain/Calculator.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorBrain
+{
+    public class Calculator
+    {
+        private string _currentValue = "0";
+        private decimal _firstValue, _secondValue, _totalValue;
+        private char _mathSymbol;
+        private Boolean _isSecondValue = false;
+        private Boolean _startSecondDisplay = false;
+        private Boolean _hasEqual = false;
+        private Boolean _symbolFlag = false;
+
+
+
+        // The current state of the calculator will have to be stored somehow
+        // in instance variables, here, declared directly inside the "Calculator"
+        // scope.
+
+        /*
+         * Valid inputs:
+         * '0' -- input the digit 0
+         * ...
+         * '9' -- input the digit 9
+         * '+' -- add
+         * '-' -- subtract
+         * '*' -- multiply
+         * '/' -- divide
+         * '=' -- perform the current calculation
+         * 'c' -- clear
+         */
+        public void ProvideInput(char input)
+        {
+            if (_hasEqual == true)
+            {
+                ResetCalc();
+            }
+            switch (input)
+            {
+                case 'C':
+                    ResetCalc();
+                    break;
+
+                case '1':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '2':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '3':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '4':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '5':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '6':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '7':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '8':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '9':
+                    CheckFlags();
+                    _currentValue = _currentValue + input.ToString();
+                    break;
+                case '0':
+                    decimal checkZero = decimal.Parse(_currentValue);
+
+                    if (checkZero != 0)
+                    {
+                        _currentValue = _currentValue + input.ToString();
+                    }
+                    break;
+                case '+':
+                    if (_hasEqual == true || _symbolFlag == true)
+                    {
+                        _totalValue = _totalValue + decimal.Parse(_currentValue);
+                        _currentValue = _totalValue.ToString();
+                    }
+                    else
+                    {
+                        if (_isSecondValue == true)
+                        {
+                            _totalValue = _firstValue + decimal.Parse(_currentValue);
+                            _currentValue = _totalValue.ToString();
+
+                        }
+                        else
+                        {
+                            _firstValue = decimal.Parse(_currentValue);
+                            _isSecondValue = true;
+                            _mathSymbol = '+';
+                            _startSecondDisplay = true;
+                            _symbolFlag = true;
+                        }
+
+                    }
+                    break;
+                case '-':
+                    if (_hasEqual == true || _symbolFlag == true)
+                    {
+                        _totalValue = _totalValue - decimal.Parse(_currentValue);
+                        _currentValue = _totalValue.ToString();
+                    }
+                    else
+                    {
+                        if (_isSecondValue == true)
+                        {
+                            _totalValue = _firstValue - decimal.Parse(_currentValue);
+                            _currentValue = _totalValue.ToString();
+
+                        }
+                        else
+                        {
+                            _firstValue = decimal.Parse(_currentValue);
+                            _isSecondValue = true;
+                            _mathSymbol = '-';
+                            _startSecondDisplay = true;
+
+
+                        }
+                    }
+                    break;
+                case '*':
+                    if (_hasEqual == true || _symbolFlag == true)
+                    {
+                        _totalValue = _totalValue * decimal.Parse(_currentValue);
+                        _currentValue = _totalValue.ToString();
+                    }
+                    else
+                    {
+                        if (_isSecondValue == true)
+                        {
+                            _totalValue = _firstValue*decimal.Parse(_currentValue);
+                            _currentValue = _totalValue.ToString();
+
+                        }
+                        else
+                        {
+                            _firstValue = decimal.Parse(_currentValue);
+                            _isSecondValue = true;
+                            _mathSymbol = '*';
+                            _startSecondDisplay = true;
+
+                        }
+                    }
+                    break;
+                case '/':
+                    if (_hasEqual == true || _symbolFlag == true)
+                    {
+                        _totalValue = _totalValue / decimal.Parse(_currentValue);
+                        _currentValue = _totalValue.ToString();
+                    }
+                    else
+                    {
+
+
+                        if (_isSecondValue == true)
+                        {
+                            _totalValue = _firstValue/decimal.Parse(_currentValue);
+                            _currentValue = _totalValue.ToString();
+
+
+                        }
+                        else
+                        {
+                            _firstValue = decimal.Parse(_currentValue);
+                            _isSecondValue = true;
+                            _mathSymbol = '/';
+                            _startSecondDisplay = true;
+
+                        }
+                    }
+
+                    break;
+                case '=':
+
+                    if (_isSecondValue == true)
+                    {
+                        switch (_mathSymbol)
+                        {
+                            case '+':
+                            {
+                                _totalValue = _firstValue + decimal.Parse(_currentValue);
+                                break;
+                            }
+                            case '-':
+                            {
+                                _totalValue = _firstValue - decimal.Parse(_currentValue);
+                                break;
+                            }
+                            case '*':
+                            {
+                                _totalValue = _firstValue * decimal.Parse(_currentValue);
+                                break;
+                            }
+                            case '/':
+                            {
+                                _totalValue = _firstValue / decimal.Parse(_currentValue);
+                                break;
+                            }
+                            default:
+                            {
+                                break; 
+                            }
+                                
+                        }
+                        
+                        _hasEqual = true;
+                        _currentValue = _totalValue.ToString();
+
+
+                    }
+
+                    break;
+                case '.':
+                    if (_currentValue.Contains("."))
+                    {
+                        //Do nothing because 2nd decimal    is invalid
+                    }
+                    else
+                    {
+                        if (_isSecondValue == true && _startSecondDisplay == true)
+                        {
+                            _currentValue = "";
+                            _startSecondDisplay = false;
+                        }
+                        _currentValue = _currentValue + input.ToString();
+                    }
+
+                    break;
+                default:
+                    break;
+
+
+            }
+   
+            
+        }
+
+        public string GetDisplay()
+        {
+
+            if (_currentValue.Contains("."))
+            {
+                return _currentValue;
+            }
+
+            var currentNumberValue = decimal.Parse(_currentValue, NumberStyles.None);
+            return currentNumberValue.ToString();
+        }
+
+        public void ResetCalc()
+        {
+            _currentValue = "0";
+            _totalValue = 0;
+            _isSecondValue = false;
+            _startSecondDisplay = false;
+            _mathSymbol = ' ';
+            _firstValue = 0;
+            _secondValue = 0;
+            _hasEqual = false;
+
+        }
+
+        public void CheckFlags()
+        {
+            if (_hasEqual == true || _symbolFlag==true)
+            {
+                _currentValue = "";
+            }
+            else
+            {
+
+                if (_isSecondValue == true && _startSecondDisplay == true)
+                {
+                    _currentValue = "";
+                    _startSecondDisplay = false;
+
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Tryon.Yamaguchi/Session 09/Calculator Multiple Math/CalculatorBrain/CalculatorUI/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CalculatorUI
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(56, 127);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(38, 38);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(56, 43);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(38, 38);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(188, 215);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(38, 38);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(56, 87);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(38, 38);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(100, 87);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(38, 38);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(56, 171);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(82, 38);
+            this.btn0.TabIndex = 2;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(100, 43);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(38, 38);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(100, 127);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(38, 38);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(144, 87);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(38, 38);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(144, 43);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(38, 38);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(144, 127);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(38, 38);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(188, 87);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(38, 38);
+            this.btnMultiply.TabIndex = 14;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(188, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(38, 38);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(188, 43);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(38, 38);
+            this.btnDivide.TabIndex = 16;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(188, 127);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(38, 38);
+            this.btnSubtract.TabIndex = 13;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(56, 215);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(82, 38);
+            this.btnEqual.TabIndex = 17;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Enabled = false;
+            this.txtDisplay.ForeColor = System.Drawing.Color.Yellow;
+            this.txtDisplay.Location = new System.Drawing.Point(56, 15);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(170, 22);
+            this.txtDisplay.TabIndex = 18;
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.Location = new System.Drawing.Point(144, 171);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(38, 38);
+            this.btnDecimal.TabIndex = 19;
+            this.btnDecimal.Text = ".";
+            this.btnDecimal.UseVisualStyleBackColor = true;
+            this.btnDecimal.Click += new System.EventHandler(this.btnDecimal_Click);
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn1);
+            this.Name = "frmCalculator";
+            this.Text = "Tryon\'s Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnDecimal;
+    }
+}
+
