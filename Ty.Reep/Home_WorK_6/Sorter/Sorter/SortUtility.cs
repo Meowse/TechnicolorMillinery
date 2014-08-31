d062a2f98019e0c8eb5eac4fdc37253ab17a0c2f
Tyreep <tyreep@tyberdude.com>
2014-08-31 15:53:39 -0700

    Homework 6

---
+++ Ty.Reep/Home_WorK_6/Sorter/Sorter/SortUtility.cs
@@ -0,0 +1,32 @@
+﻿namespace Sorter
+{
+    public class SortUtility
+    {
+        public void Sort(int[] values)
+        {
+            // Read a value with "values[3]"
+            // Write a value with "values[2] = 17"
+            // Get the number of values with "values.Length"
+
+            // First value in the array is "values[0]"
+            // Last value in the array is "values[values.Length - 1]"
+
+            // Example of using a for-loop to double all elements of an array
+            if (values.Length >1)
+                {
+                if (values[0] > values[1])
+                    {
+                        int temp = values[0];
+                        values[0] = values[1];
+                        values[1] = temp;
+                    }
+                }
+            if (values.Length > 2)
+            {
+                int temp = values[1];
+                values[1] = values[2];
+                values[2] = temp;
+            }
+        }
+    }
+}
